15e56a8772fa28dc92f26eebcd980e20cc46d220
Nikodemus Siivola <nikodemus@random-state.net>
2011-11-03 13:38:54 +0200

    add sb-texinfo.asd

--- docstrings.lisp
+++ docstrings.lisp
@@ -1,15 +1,14 @@
 ;;; -*- lisp -*-
 
-;;;; A docstring extractor for the sbcl manual.  Creates
-;;;; @include-ready documentation from the docstrings of exported
-;;;; symbols of specified packages.
+;;;; A docstring extractor for the SBCL. Creates @include-ready documentation
+;;;; from the docstrings of exported symbols of specified packages.
 
-;;;; This software is part of the SBCL software system. SBCL is in the
-;;;; public domain and is provided with absolutely no warranty. See
-;;;; the COPYING file for more information.
+;;;; This software is part of the SBCL software system. SBCL is in the public
+;;;; domain and is provided with absolutely no warranty. See the COPYING file
+;;;; for more information.
 ;;;;
 ;;;; Written by Rudi Schlatte <rudi@constantly.at>, mangled
-;;;; by Nikodemus Siivola.
+;;;; by Nikodemus Siivola <nikodemus@random-state.net>.
 
 ;;;; TODO
 ;;;; * Verbatim text
@@ -35,9 +34,6 @@
 ;;;;
 ;;;; Lines containing only a SYMBOL that are followed by indented
 ;;;; lines are marked up as @table @code, with the SYMBOL as the item.
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (require 'sb-introspect))
 
 (defpackage :sb-texinfo
   (:use :cl :sb-mop)
