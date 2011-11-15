d88215903747185b757b7dcaaa2395a167d602fb
Eric Schulte <schulte.eric@gmail.com>
2011-11-15 16:27:22 -0700

    tweaks

--- gcd/options.lisp
+++ gcd/options.lisp
@@ -11,6 +11,6 @@
  *SEED-SOFT*           "gcd.s"     ;; File holding a seed individual
  *SAVE-SOFT*           "fix.s"     ;; File to hold any potential individual returned by `evolve'.
  *SAVE-POPULATION*     "pop.store" ;; Save the final population here.
- MAX-EVALS             1000        ;; quit after this many fitness evaluations
+ MAX-EVALS             100         ;; quit after this many fitness evaluations
  MAX-FIT               11          ;; quit when an individual achieves this fitness or higher
  )
