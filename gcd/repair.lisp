533ec05daa94a9dab5c967ba5c438367652e7b0b
Eric Schulte <eric.schulte@gmx.com>
2011-12-16 09:25:03 -0700

    extracting from `let'

--- gcd/repair.lisp
+++ gcd/repair.lisp
@@ -5,21 +5,25 @@
 ;; compile gcd.c to assembly
 (shell "gcc -S gcd.c")
 
-(let ((*pos-test-num* 5)
-      (*neg-test-num* 1)
-      (*max-population-size* 100)
-      (*genome-averaging-keys* '(:pos :neg))
-      (gcd (asm-from-file "gcd.s"))
-      (fixed-program-path "fixed.s")
-      repair)
-  ;; add the good and bad path information to gcd
-  (apply-path gcd :neg (samples-from-oprofile-file "sample.neg"))
-  (apply-path gcd :pos (samples-from-oprofile-file "sample.pos"))
-  ;; build up a starting population -- 8 copies of the original
-  (dotimes (_ 8) (push gcd *population*))
-  ;; evolve a repair
-  (setq repair (evolve :max-evals 1000 :max-fit 6))
-  (if repair
+(setq *pos-test-num* 5)
+(setq *neg-test-num* 1)
+(setq *max-population-size* 100)
+(setq *genome-averaging-keys* '(:pos :neg))
+(setq original (asm-from-file "gcd.s"))
+
+;; add the good and bad path information to gcd
+;; (apply-path original :neg (samples-from-oprofile-file "sample.neg"))
+;; (apply-path original :pos (samples-from-oprofile-file "sample.pos"))
+
+;; build up a starting population -- 8 copies of the original
+(dotimes (_ 8) (push original *population*))
+
+;; evolve a repair
+(defvar *repair* (evolve :max-evals 1000 :max-fit 6))
+
+;; conditionally save the repair
+(let ((fixed-program-path "fixed.s"))
+  (if *repair*
       (progn (format t "~&Repair found, saving to ~a~%" fixed-program-path)
-             (asm-to-file repair fixed-program-path))
+             (asm-to-file *repair* fixed-program-path))
       (format t "~&No repair found.~%")))
