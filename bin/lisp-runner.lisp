8c8c5dabef5bd8664880fc5cd2e41d082e5b7d15
Eric Schulte <schulte.eric@gmail.com>
2013-03-22 12:30:39 -0600

    removing bin/

--- bin/lisp-runner.lisp
+++
@@ -1,6 +0,0 @@
-(defun main (argv)
-  (format t "~a~%"
-          (eval (read-from-string
-                 (if (third argv)
-                     (progn (load (second argv)) (third argv))
-                     (second argv))))))
