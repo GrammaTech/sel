f32d89b786134bffd512f0fc85dc8735179ed5fd
Eric Schulte <schulte.eric@gmail.com>
2014-01-17 21:23:02 -0700

    command line helpers for software evolution

---
+++ examples/command-line.lisp
@@ -0,0 +1,31 @@
+;;; command-line.lisp --- Command line helpers for software evolution
+
+;; Copyright (C) 2011-2013  Eric Schulte
+
+;;; License: GNU General Public License, Version 3 or later
+
+;;; Commentary:
+
+;;; Code:
+(defpackage :software-evolution-command-line
+  (:use :common-lisp :metabang-bind)
+  (:export :quit :throw-error :getopts))
+(in-package :software-evolution-command-line)
+
+(defun quit (&optional (errno 0))
+  #+sbcl (sb-ext:exit :code errno)
+  #+ccl  (ccl:quit errno))
+
+(defun throw-error (&rest args)
+  (apply #'format *error-output* args)
+  (quit))
+
+(defmacro getopts (&rest forms)
+  (let ((arg (gensym)))
+    `(loop :for ,arg = (pop args) :while ,arg :do
+        (cond
+          ,@(mapcar (lambda-bind ((short long . body))
+                      `((or (and ,short (string= ,arg ,short))
+                            (and ,long  (string= ,arg ,long)))
+                        ,@body))
+                    forms)))))
---
+++ examples/software-evolution-command-line.asd
@@ -0,0 +1,5 @@
+(defsystem :software-evolution-command-line
+  :description "command line helpers"
+  :licence "GPL V3"
+  :depends-on (metabang-bind)
+  :components ((:file "command-line")))
