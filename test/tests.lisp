b3b969a3c3be200ec3512fefcb6554057f14823b
Eric Schulte <schulte.eric@gmail.com>
2014-02-07 14:11:08 -0700

    testing the elf representation

--- test/tests.lisp
+++ test/tests.lisp
@@ -60,6 +60,15 @@
   (:setup (setf *gcd* (from-file (make-instance 'asm) (gcd-dir "gcd.s"))))
   (:teardown (setf *gcd* nil)))
 
+(defixture gcd-elf
+  (:setup
+   (let ((arch (intern (string-upcase (subseq (shell "uname -m") 0 3)))))
+     (setf *gcd* (from-file (make-instance (case arch
+                                             (x86 'elf-x86-sw)
+                                             (mips 'elf-mips-sw)))
+                            (gcd-dir "gcd")))))
+  (:teardown (setf *gcd* nil)))
+
 (defixture population
   (:setup (setf *population* (loop for i from 1 to 9
                                 collect (make-instance 'soft
@@ -139,6 +148,77 @@
         ;; (is (some [{equal :crossover} #'car] (edits new)))
         ;; (is (some [{equal :cut} #'caar] (second (edits new))))
         ))))
+
+
+;;; ELF representation
+(defun bytes (elf) (mapcan [#'cdr {assoc :bytes}] (copy-tree (genome elf))))
+
+(deftest elf-read ()
+  (with-fixture gcd-elf
+    (is (or (equal 'elf-x86-sw (type-of *gcd*))
+            (equal 'elf-mips-sw (type-of *gcd*))))))
+
+(deftest elf-idempotent-read-write ()
+  (with-temp-file (a)
+    (with-fixture gcd-elf
+      (phenome *gcd* :bin a)
+      (multiple-value-bind (out err ret)
+          (software-evolution::shell "diff ~s ~a"
+                                     (namestring (gcd-dir "gcd")) a)
+        (declare (ignorable out err))
+        (is (= 0 ret))))))
+
+(deftest elf-copy-same-genome ()
+  (with-fixture gcd-elf
+   (is (software-evolution::equal-it (genome *gcd*)
+                                     (genome (copy *gcd*))))))
+
+(deftest elf-idempotent-read-copy-write ()
+  (with-temp-file (a)
+    (with-fixture gcd-elf
+      (phenome (copy *gcd*) :bin a)
+      (multiple-value-bind (out err ret)
+          (software-evolution::shell "diff ~s ~a"
+                                     (namestring (gcd-dir "gcd")) a)
+        (declare (ignorable out err))
+        (is (= 0 ret))))))
+
+(deftest elf-edit-of-copy-does-not-change-original ()
+  (with-fixture gcd-elf
+    (let ((orig-hash (sxhash (genome *gcd*)))
+          (ant (copy *gcd*)))
+      (mutate ant)
+      (is (not (software-evolution::equal-it (genome ant) (genome *gcd*))))
+      (is (equal orig-hash (sxhash (genome *gcd*)))))))
+
+(deftest elf-cut-changes-but-maintains-byte-length ()
+  (with-fixture gcd-elf
+    (let ((variant (copy *gcd*)))
+      (apply-mutation variant '(:cut 4))
+      (is (= (length (bytes *gcd*)) (length (bytes variant))))
+      (is (not (equal-it (bytes *gcd*) (bytes variant)))))))
+
+(deftest elf-insertion-changes-but-maintains-lengthens ()
+  (with-fixture gcd-elf
+    (let ((variant (copy *gcd*)))
+      (apply-mutation variant '(:insert 4 8))
+      (is (= (length (bytes *gcd*)) (length (bytes variant))))
+      (is (not (equal-it (bytes *gcd*) (bytes variant)))))))
+
+(deftest elf-swap-changes-but-maintains-length ()
+  (with-fixture gcd-elf
+    (let ((variant (copy *gcd*)))
+      (apply-mutation variant '(:swap 4 8))
+      (is (= (length (bytes *gcd*)) (length (bytes variant))))
+      (is (not (equal-it (bytes *gcd*) (bytes variant)))))))
+
+(deftest elf-crossover-test ()
+  (with-fixture gcd-elf
+    (let ((variant (copy *gcd*)))
+      (apply-mutation variant '(:cut 0))
+      (let ((new (crossover variant *gcd*)))
+        (is (not (equal-it (genome new) (genome *gcd*))))
+        (is (= (length (bytes *gcd*)) (length (bytes variant))))))))
 
 
 ;;; Range representation
