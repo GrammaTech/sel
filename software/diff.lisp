404af67a61484de9c9b2268de11c5c17b5667748
eschulte <eschulte@9ff7c0e9-86fd-0310-97a7-93b0df45db14>
2014-11-12 20:42:01 +0000

    diff rep appears initially functional
      ok eschulte@ ip:DHS-genpatcher
    
    git-svn-id: svn+ssh://svn/svn/trunk/third-party/software-evolution@104164 9ff7c0e9-86fd-0310-97a7-93b0df45db14

--- software/diff.lisp
+++ software/diff.lisp
@@ -9,7 +9,7 @@
 ;;; Code:
 (in-package :software-evolution)
 
-(defclass diff (simple)
+(defclass sw-diff (simple)
   ((reference :initarg :reference :accessor reference :initform nil)
    (genome    :initarg :genome    :initform nil)
    (diffs     :initarg :diffs     :accessor diffs     :initform nil))
@@ -20,24 +20,25 @@ of range references to an external REFERENCE code array.
 
 Similar to the range approach, but striving for a simpler interface."))
 
-(defmethod copy :around ((diff diff))
-  ;; TODO: Wrap the next methods to copy reference and diffs instead
-  ;; of the genome.
-  (let ((ref (reference diff))
-        (diffs (diffs diff)))
-    (setf (reference diff) nil)
-    (setf (diffs diff) (make-instance 'diff::diff))
+(defvar *in-copy* nil
+  "Cheating: set to non-nil only when mid-copy, at which point the
+genome methods should have no effect.")
+
+(defmethod copy :around ((diff sw-diff))
+  (let ((*in-copy* t))
     (let ((copy (call-next-method)))
-      (setf (reference copy) ref)
-      (setf (diffs copy) (copy diffs)))))
+      (setf (reference copy) (reference diff))
+      (setf (diffs copy)     (diffs diff))
+      copy)))
 
-(defmethod genome ((diff diff))
+(defmethod genome ((diff sw-diff))
   ;; Build the genome on the fly from the reference and diffs
-  (with-slots (reference diffs) diff
-    (apply-seq-diff reference diffs)))
+  (unless *in-copy*
+    (with-slots (reference diffs) diff
+      (apply-seq-diff reference diffs))))
 
-(defmethod (setf genome) ((diff diff) new)
+(defmethod (setf genome) (new (diff sw-diff))
   ;; Convert the genome to a set of diffs against the reference
-  (assert (listp new) (new) "Diff genomes must be lists.")
-  (with-slots (reference diffs) diff
-    (setf diffs (generate-seq-diff 'unified-diff reference new))))
+  (unless *in-copy*
+    (with-slots (reference diffs) diff
+      (setf diffs (generate-seq-diff 'unified-diff reference new)))))
