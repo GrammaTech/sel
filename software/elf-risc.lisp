896a72a443a367fe81200f3e9e9c1350b50e1058
eschulte <eschulte@9ff7c0e9-86fd-0310-97a7-93b0df45db14>
2014-11-12 20:41:41 +0000

    mips is now just a special case of risc

--- software/elf-risc.lisp
+++ software/elf-risc.lisp
@@ -1,4 +1,4 @@
-;;; elf-mips.lisp --- software representation of mips ELF files
+;;; elf-risc.lisp --- software representation of risc ELF files
 
 ;; Copyright (C) 2011-2013  Eric Schulte
 
@@ -11,17 +11,17 @@
 
 
 ;;; elf software objects
-(defclass elf-mips-sw (elf-sw) ())
+(defclass elf-risc-sw (elf-sw) ())
 
-(defvar mips-nop #x0)
+(defvar risc-nop #x0)
 
-(defmethod copy ((elf elf-mips-sw))
+(defmethod copy ((elf elf-risc-sw))
   (make-instance (type-of elf)
     :fitness (fitness elf)
     :genome (map 'vector #'copy-tree (coerce (genome elf) 'list))
     :base (base elf)))
 
-(defmethod elf ((elf elf-mips-sw))
+(defmethod elf ((elf elf-risc-sw))
   (with-slots (base genome) elf
     (let ((new (copy-elf base))
           (offset 0))
@@ -33,7 +33,7 @@
                            (sections new)))
       new)))
 
-(defmethod from-file ((elf elf-mips-sw) path)
+(defmethod from-file ((elf elf-risc-sw) path)
   (with-slots (base genome) elf
     (setf base (read-elf path)
           genome
@@ -46,13 +46,13 @@
            'vector)))
   elf)
 
-(defmethod lines ((elf elf-mips-sw))
+(defmethod lines ((elf elf-risc-sw))
   (map 'list {aget :bytes} (genome elf)))
 
-(defmethod (setf lines) (new (elf elf-mips-sw))
+(defmethod (setf lines) (new (elf elf-risc-sw))
   (setf (genome elf) (map 'vector [#'list {cons :bytes}] new)))
 
-(defmethod apply-mutation ((elf elf-mips-sw) mut)
+(defmethod apply-mutation ((elf elf-risc-sw) mut)
   (let ((starting-length (length (genome elf))))
     (setf (genome elf)
           (ecase (car mut)
@@ -65,20 +65,20 @@
             (elf) "mutation ~S changed size of genome [~S -> ~S]"
             mut starting-length (length (genome elf)))))
 
-(defmethod elf-cut ((elf elf-mips-sw) s1)
+(defmethod elf-cut ((elf elf-risc-sw) s1)
   (with-slots (genome) elf
-    (setf (cdr (assoc :bytes (aref genome s1))) mips-nop)
+    (setf (cdr (assoc :bytes (aref genome s1))) risc-nop)
     genome))
 
-;; Thanks to the uniform width of MIPS instructions, this is the only
+;; Thanks to the uniform width of RISC instructions, this is the only
 ;; operation which requires any bookkeeping.  We'll try to 
-(defvar elf-mips-max-displacement nil
+(defvar elf-risc-max-displacement nil
   "Maximum range that `elf-insert' will displace instructions.
 This is the range within which insertion will search for a nop to
 delete, if none is found in this range insertion becomes replacement.
 A value of nil means never replace.")
 
-(defmethod elf-insert ((elf elf-mips-sw) s1 val)
+(defmethod elf-insert ((elf elf-risc-sw) s1 val)
   (with-slots (genome) elf
     (let* ((borders (reduce (lambda (offsets ph)
                               (cons (+ (car offsets) (filesz ph))
@@ -86,7 +86,7 @@ A value of nil means never replace.")
                             (program-table (base elf)) :initial-value '(0)))
            (backwards-p t) (forwards-p t)
            (nop-location               ; find the nearest nop in range
-            (loop :for i :below (or elf-mips-max-displacement infinity) :do
+            (loop :for i :below (or elf-risc-max-displacement infinity) :do
                (cond
                  ;; don't cross borders
                  ((member (+ s1 i) borders) (setf forwards-p nil))
@@ -94,10 +94,10 @@ A value of nil means never replace.")
                  ((and (not forwards-p) (not backwards-p)) (return nil))
                  ;; continue search forwards and backwards
                  ((and forwards-p
-                       (= mips-nop (cdr (assoc :bytes (aref genome (+ s1 i))))))
+                       (= risc-nop (cdr (assoc :bytes (aref genome (+ s1 i))))))
                   (return (+ s1 i)))
                  ((and backwards-p
-                       (= mips-nop (cdr (assoc :bytes (aref genome (- s1 i))))))
+                       (= risc-nop (cdr (assoc :bytes (aref genome (- s1 i))))))
                   (return (- s1 i)))))))
       (if nop-location                 ; displace all bytes to the nop
           (reduce (lambda (previous i)
@@ -108,7 +108,7 @@ A value of nil means never replace.")
           (setf (cdr (assoc :bytes (aref genome s1))) val)))
     genome))
 
-(defmethod elf-swap ((elf elf-mips-sw) s1 s2)
+(defmethod elf-swap ((elf elf-risc-sw) s1 s2)
   (with-slots (genome) elf
     (let ((left-bytes  (copy-tree (cdr (assoc :bytes (aref genome s1)))))
           (right-bytes (copy-tree (cdr (assoc :bytes (aref genome s2))))))
@@ -116,7 +116,7 @@ A value of nil means never replace.")
             (cdr (assoc :bytes (aref genome s2))) left-bytes))
     genome))
 
-(defmethod crossover ((a elf-mips-sw) (b elf-mips-sw))
+(defmethod crossover ((a elf-risc-sw) (b elf-risc-sw))
   "One point crossover."
   (let ((point (random (length (genome a))))
         (new (copy a)))
