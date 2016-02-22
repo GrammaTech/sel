02c8f08c3814f1d6f974815b32f87128d14f74fa
Matt Noonan <mnoonan@grammatech.com>
2016-02-22 12:17:45 -0500

    Bugfixes and unit tests for function rebinding.
    This fixes some problems when using function rebinding with crossover,
    and adds a unit test to ensure that the helper random-function-name can
    find a function with the correct arity.

--- utility/utility.lisp
+++ utility/utility.lisp
@@ -759,10 +759,12 @@ that function may be declared.")
                (setf (gethash x (or ht new-ht)) t)))))
     (or ht new-ht)))
 
-(defun ht->list (ht)
+(defun ht->list (ht &key merge-fn)
   (loop for k being the hash-keys of ht
      using (hash-value v)
-     collecting (if (eq v t) k (list k v))))
+     collecting (if (eq v t) k (if merge-fn
+                                   (funcall merge-fn k v)
+                                   (list k v)))))
 
 (defun merge-hash-tables (to-ht from-ht &optional with)
   (labels ((merge-fn (x y) (if with (funcall with  x y) x)))
