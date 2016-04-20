c66e90aa46ee325e0e5348bd704948acb1666413
Matt Noonan <mnoonan@grammatech.com>
2016-04-20 09:41:46 -0400

    Missed a merge conflict region.

--- software/clang.lisp
+++ software/clang.lisp
@@ -1000,16 +1000,9 @@ free variables.")
            (the-block (enclosing-block clang decl))
            (old-names (aget :declares (get-ast clang decl)))
            (uses (apply #'append
-<<<<<<< Updated upstream
-                        (mapcar (lambda (name)
-                                  (get-children-using clang name the-block))
-                                old-names)))
-           (vars (remove-if (lambda (var) (find var old-names :test #'equal))
-=======
-                        (mapcar (lambda (x) (get-children-using clang x the-block))
-                                old-names)))
+                    (mapcar (lambda (x) (get-children-using clang x the-block))
+                            old-names)))
            (vars (remove-if (lambda (x) (find x old-names :test #'equal))
->>>>>>> Stashed changes
                             (get-vars-in-scope clang
                                                (if uses (car uses) the-block))))
            (var (loop :for _ :in old-names :collecting
