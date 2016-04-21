8e66cd6e90b00cb689ab9d492feffe8109249c3a
Eric Schulte <eschulte@grammatech.com>
2016-04-21 16:13:59 -0400

    change order so dependencies are satisfied

--- software-evolution.lisp
+++ software-evolution.lisp
@@ -243,22 +243,10 @@ elements.")
   (:documentation "Apply MUTATION to SOFTWARE.
 Define an :around method on this function to record mutations."))
 
-(defmethod apply-mutation :before ((obj software) (mut mutation))
-  ;; Mutation removes previously calculated fitness values.
-  (declare (ignorable mutation))
-  (setf (fitness obj) nil))
-
 (defgeneric apply-mutations (software mutation)
   (:documentation "Apply MUTATION to every target in SOFTWARE.
 Returns the resulting software objects, also returns a list of the
 applied mutations."))
-
-(defmethod apply-mutations ((obj software) (mut mutation))
-  (setf (object mut) obj)
-  (loop :for targeted :in (mapcar {at-targets mut} (targets mut))
-     :collect targeted :into mutations
-     :collect (apply-mutation (copy obj) targeted) :into results
-     :finally (return (values results mutations))))
 
 (defgeneric crossover (software-a software-b)
   (:documentation "Crossover two software objects.
@@ -338,6 +326,18 @@ Also, ensures MUTATION is a member of superclasses"
 
 (defmethod mutation-key ((obj software) (mutation mutation))
   (declare (ignorable obj)) (type-of mutation))
+
+(defmethod apply-mutation :before ((obj software) (mut mutation))
+  ;; Mutation removes previously calculated fitness values.
+  (declare (ignorable mutation))
+  (setf (fitness obj) nil))
+
+(defmethod apply-mutations ((obj software) (mut mutation))
+  (setf (object mut) obj)
+  (loop :for targeted :in (mapcar {at-targets mut} (targets mut))
+     :collect targeted :into mutations
+     :collect (apply-mutation (copy obj) targeted) :into results
+     :finally (return (values results mutations))))
 
 
 ;;; Evolution
