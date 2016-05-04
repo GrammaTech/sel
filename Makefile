31184733e9465542e681d8c244a05a06ffd5b810
Eric Schulte <eschulte@grammatech.com>
2016-05-04 15:12:22 -0400

    initial commit

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
-.PHONY: check doc test tests.md auto-check
+.PHONY:
+.PRECIOUS:
+
+# Set personal or machine-local flags in a file named local.mk
+ifneq ("$(wildcard local.mk)","")
+include local.mk
+endif
+
+## Initial Compiler and Flags
+CC ?= clang
+CFLAGS ?= -O0
+
+all: bin/clang-instrument
+
+
+## Executables
 
 # Use buildapp as the lisp compiler.
 LC ?= buildapp
@@ -12,20 +27,18 @@ $(warning $(QUICK_LISP) does not appear to be a valid quicklisp install)
 $(error Please point QUICK_LISP to your quicklisp installation)
 endif
 
-LISP_LIBS+= software-evolution-test
+MANIFEST_FILE=$(QUICK_LISP)/local-projects/system-index.txt
+LISP_LIBS+= clang-instrument
 LC_LIBS:=$(addprefix --load-system , $(LISP_LIBS))
-
-LISP_DEPS =				\
-	$(wildcard *.lisp) 		\
-	$(wildcard software/*.lisp)	\
-	$(wildcard test/*.lisp)
+LOADED_LIBS_TMP:=$(addprefix $(QUICK_LISP)/local-projects/, $(LISP_LIBS))
+LOADED_LIBS:=$(LOADED_LIBS_TMP:=.loaded)
 
 # Flags to buildapp
 QUIT=(lambda (error hook-value)
 QUIT+=(declare (ignorable hook-value))
 QUIT+=(format *error-output* \"ERROR: ~a~%\" error)
 QUIT+=\#+sbcl (sb-ext:exit :code 2) \#+ccl (quit 2))
-LCFLAGS=--manifest-file $(QUICK_LISP)/local-projects/system-index.txt \
+LCFLAGS=--manifest-file $(MANIFEST_FILE) \
 	--asdf-tree $(QUICK_LISP)/dists/quicklisp/software \
 	--eval "(setf *debugger-hook* $(QUIT))"
 
@@ -33,42 +46,45 @@ ifneq ($(LISP_STACK),)
 LCFLAGS+= --dynamic-space-size $(LISP_STACK)
 endif
 
-se-test: $(LISP_DEPS)
-	$(LC) $(LCFLAGS) $(LC_LIBS) --output $@ \
-		--entry "software-evolution-test:batch-test"
+# Default lisp to build manifest file.
+SBCL ?= sbcl
+ifeq ("$(LISP)","")
+ifeq ("$(SBCL_HOME)","")
+SBCL_HOME=$(dir $(shell which $(SBCL)))../lib/sbcl
+endif
+LISP = SBCL_HOME=$(SBCL_HOME) $(SBCL)
+endif
+LISP ?= sbcl
+ifneq (,$(findstring sbcl, $(LISP)))
+LISP_FLAGS = --no-userinit --no-sysinit
+else
+LISP_FLAGS = --quiet --no-init
+endif
+
+$(MANIFEST_FILE): $(wildcard $(QUICK_LISP)/local-projects/*/*.asd)
+	$(LISP) $(LISP_FLAGS) --load $(QUICK_LISP)/setup.lisp --eval '(ql:register-local-projects)' \
+          --eval "#+sbcl (exit) #+ccl (quit)"
+
+%.loaded:
+	$(LISP) $(LISP_FLAGS) --load $(QUICK_LISP)/setup.lisp --eval '(ql:quickload :$(notdir $*))' \
+          --eval "#+sbcl (exit) #+ccl (quit)"
+	touch $@
+
+bin/clang-instrument: src/clang-instrument.lisp $(LOADED_LIBS) $(MANIFEST_FILE)
+	CC=$(CC) $(LC) $(LCFLAGS) $(LC_LIBS) --output $@ --entry "ci:main"
 
 
-## Documentation
-doc:
-	make -C doc
+# Documentation
+clang-instrument.1.gz: README.md
+	pandoc -s -t man $^ -o $<.tmp
+	gzip -9 < $<.tmp > $@
+
+index.html: README.md
+	pandoc -s -t html $< -o $@
 
 
-## Testing
-test:
-	@make -sC test
-
-check: se-test test
-	@./$<
-
-# Makefile target to support automated testing.
-tests.md: se-test test
-	echo "### $$(date +%Y-%m-%d-%H-%M-%S)" >> tests.md
-	echo "REPO" >> tests.md
-	echo ":   $(REPO)" >> tests.md
-	echo "" >> tests.md
-	echo "BRANCH" >> tests.md
-	echo ":   $(BRANCH)" >> tests.md
-	echo "" >> tests.md
-	echo "HEAD" >> tests.md
-	echo ":   $(HEAD)" >> tests.md
-	echo "" >> tests.md
-	make -s check 2>&1|sed 's/^/    /' >> tests.md
-	echo "" >> tests.md
-
-tests.html: tests.md
-	markdown $< > $@
-
-auto-check: tests.html
-
+# Administrative stuff
 clean:
-	rm -f se-test
+	@rm -f bin/clang-instrument
+	@rm -f src/*.fasl
+	@rm -f *{.md.tmp,.html,.1.gz}
---
+++ src/clang-instrument.lisp
@@ -0,0 +1,117 @@
+;;; clang-instrument --- Instrument C-language source files
+(in-package :clang-instrument)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (enable-curry-compose-reader-macros :include-utf8))
+
+(defvar *uninstrumented-classes* '("ParmVar" "CompoundStmt")
+  "AST classes which should not be instrumented.")
+
+(defvar *uninstrumented-parent-classes*
+  nil
+  "AST classes which should not be instrumented.")
+
+(defmethod instrument ((obj clang) &optional trace-file)
+  (when trace-file (warn "TODO: implement TRACE-FILE support."))
+  (-<>> (asts obj)
+        (remove-if-not {aget :full-stmt})
+        (remove-if [{member _ *uninstrumented-classes* :test #'string=}
+                    {aget :ast-class}])
+        (mapcar
+         (lambda (ast)
+           (let ((counter (aget :counter ast)))
+             `(:set
+               (:stmt1 . ,counter)
+               (:value1 . ,(format nil "fputs(\"TRACE:~d\\n\", stderr);~%~a"
+                                   counter
+                                   (peel-bananas (aget :src-text ast))))))))
+        (sort <> #'> :key [{aget :stmt1} #'cdr])
+        (mapc (lambda (op)
+                (note 2 "Instrumenting AST#~d." (aget :stmt1 (cdr op)))
+                (setf (genome obj) (clang-mutate obj op))
+                obj)))
+  obj)
+
+(defmacro getopts (&rest forms)
+  (let ((arg (gensym)))
+    `(loop :for ,arg = (pop args) :while ,arg :do
+        (cond
+          ,@(mapcar (lambda-bind ((short long . body))
+                      `((or (string= ,arg ,short) (string= ,arg ,long)) ,@body))
+                    forms)
+          (:otherwise (error "Unrecognized argument:~a" ,arg))))))
+
+(setf *note-level* 1)
+
+(defun main (args)
+  "Interface to the command line instrumentation tool."
+  (in-package :clang-instrument)
+  (let ((self (pop args))
+        (original (make-instance 'clang
+                    :compiler (or (getenv "CC") "clang")
+                    :flags (getenv "CFLAGS")))
+        path out-dir name type trace-file out-file save-original)
+    (when (or (not args)
+              (< (length args) 1)
+              (string= (subseq (car args) 0 2) "-h")
+              (string= (subseq (car args) 0 3) "--h"))
+      (format t "Usage: ~a SOURCE [OPTIONS]
+ Instrument SOURCE along OPTIONS.
+
+Options:
+   ,--compiler CC ---------- use CC as the C compiler
+                             (default to CC env. variable or clang)
+ -f,--file FILE ------------ instrumented to write trace to fILE
+                             (default to STDOUT)
+ -F,--flags FLAGS ---------- comma-separated list of compiler flags
+                             (default to CFLAGS env. variable)
+ -o,--out-file FILE -------- write mutated source to FILE
+                             (default STDOUT)
+ -O,--orig ----------------- also save a copy of the original
+ -q,--quiet ---------------- set verbosity level to 0
+ -v,--verbose NUM ---------- verbosity level 0-4
+
+Built with ~a version ~a.~%"
+              self (lisp-implementation-type) (lisp-implementation-version))
+      (sb-ext:exit))
+
+    ;; Mandatory arguments.
+    (setf path (pop args)
+          out-dir (pathname-directory path)
+          name (pathname-name path)
+          type (pathname-type path)
+          original (from-file original path))
+
+    ;; Options.
+    (getopts
+      (nil "--compiler" (setf (compiler original) (pop args)))
+      ("-f" "--file" (setf trace-file (pop args)))
+      ("-F" "--flags" (setf (flags original) (pop args)))
+      ("-o" "--out-file" (setf out-file (pop args)))
+      ("-O" "--orig" (setf save-original t))
+      ("-q" "--quiet" (setf *note-level* 0))
+      ("-v" "--verbose"   (let ((lvl (parse-integer (pop args))))
+                            (when (>= lvl 4) (setf *shell-debug* t))
+                            (setf *note-level* lvl))))
+
+    ;; Save original.
+    (when save-original
+      (let ((dest (make-pathname
+                   :directory out-dir
+                   :name (format nil "~a-original" name)
+                   :type type)))
+        (note 1 "Saving original to ~a." dest)
+        (with-open-file (out dest :direction :output :if-exists :supersede)
+          (genome-string original out))))
+
+    ;; Instrument and save.
+    (note 1 "Instrumenting ~a." path)
+    (let ((dest (or out-file
+                    (make-pathname
+                     :directory out-dir
+                     :name (format nil "~a-instrumented" name)
+                     :type type)))
+          (instrumented (clang-format (instrument original trace-file))))
+      (note 1 "Writing instrumented to ~a." dest)
+      (with-open-file
+          (out dest :direction :output :if-exists :supersede)
+        (genome-string instrumented out)))))
