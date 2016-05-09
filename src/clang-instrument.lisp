689b85ef31f410f75ff7871d7f3d35694a049cbd
Eric Schulte <eschulte@grammatech.com>
2016-05-09 14:17:45 -0400

    instrumentation supports an optional trace file
      This commit also updates the README and markdown documentation.

--- src/clang-instrument.lisp
+++ src/clang-instrument.lisp
@@ -3,32 +3,79 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (enable-curry-compose-reader-macros :include-utf8))
 
+
+;;;; Instrumentation
 (defmethod instrument ((obj clang) &optional trace-file)
-  (when trace-file (warn "TODO: implement TRACE-FILE support."))
-  (-<>> (asts obj)
-        (remove-if-not {aget :full-stmt})
-        ;; Remove statements if they are *not* in a compound statement.
-        (remove-if-not
-         (lambda (ast)
-           (let ((parent-counter (aget :parent-counter ast)))
-             (and (not (zerop parent-counter))
-                  (string= "CompoundStmt"
-                           (aget :ast-class (get-ast obj parent-counter)))))))
-        (mapcar {aget :counter})
-        (sort <> #'>)
-        (reduce (lambda (variant counter)
-                  (note 2 "Instrumenting AST#~d." counter)
-                  (let* ((fmt "fputs(\"((:COUNTER . ~d))\\n\", stderr);~%~a")
-                         (text (peel-bananas
-                                (aget :src-text (get-ast variant counter))))
-                         (op `(:set
-                               (:stmt1 . ,counter)
-                               (:value1 . ,(format nil fmt counter text)))))
-                    (setf (genome variant) (clang-mutate variant op)))
-                  (update-asts variant)
-                  variant)
-                <> :initial-value (copy obj))))
+  (let ((log-var (if trace-file "__bi_mut_log_file" "stderr")))
+    (-<>> (asts obj)
+          (remove-if-not {aget :full-stmt})
+          ;; Remove statements if they are *not* in a compound statement.
+          (remove-if-not
+           (lambda (ast)
+             (let ((parent-counter (aget :parent-counter ast)))
+               (and (not (zerop parent-counter))
+                    (string=
+                     "CompoundStmt"
+                     (aget :ast-class (get-ast obj parent-counter)))))))
+          (mapcar {aget :counter})
+          (sort <> #'>)
+          (reduce
+           (lambda (variant counter)
+             (note 2 "Instrumenting AST#~d." counter)
+             (let* ((fmt "fputs(\"((:COUNTER . ~d))\\n\", ~a);~%~a")
+                    (text (peel-bananas
+                           (aget :src-text (get-ast variant counter))))
+                    (op
+                     `(:set
+                       (:stmt1 . ,counter)
+                       ,(cons :value1
+                              (format nil fmt counter log-var text)))))
+               (setf (genome variant) (clang-mutate variant op)))
+             (update-asts variant)
+             variant)
+           <> :initial-value obj)
+          (update-asts)
+          (setf obj))
+    (when trace-file
+      (setf obj (log-to-filename obj log-var trace-file)))
+    (clang-format obj)
+    obj))
 
+(defgeneric get-entry (software)
+  (:documentation "Return the counter of the entry AST in SOFTWARE."))
+
+(defmethod get-entry ((obj clang))
+  (first (aget :stmt-range
+               (find-if [{string= "main"} {aget :name}] (prototypes obj)))))
+
+(defgeneric insert-at-entry (software ast)
+  (:documentation "Insert AST at the entry point to SOFTWARE."))
+
+(defmethod insert-at-entry ((obj clang) (ast string))
+  ;; NOTE: The most robust way to perform this calculation is to grab
+  ;; the whole text for the entry AST (i.e., "main"), and then to
+  ;; insert the new text just after the first "{".
+  (let* ((entry (get-entry obj))
+         (old-text
+          (peel-bananas (subseq (aget :src-text (get-ast obj entry)) 1))))
+    (setf
+     (genome obj)
+     (clang-mutate obj
+       `(:set
+         (:stmt1 . ,entry)
+         (:value1 . ,(concatenate 'string "{" ast old-text))))))
+  obj)
+
+(defmethod log-to-filename ((obj clang) log-variable filename)
+  (insert-at-entry
+   obj
+   (concatenate 'string
+     (format nil "FILE *~a;~%" log-variable)
+     (format nil "~a = fopen(~s, \"a\");~%" log-variable filename)))
+  obj)
+
+
+;;;; Command line
 (defmacro getopts (&rest forms)
   (let ((arg (gensym)))
     `(loop :for ,arg = (pop args) :while ,arg :do
@@ -56,16 +103,16 @@
  Instrument SOURCE along OPTIONS.
 
 Options:
-   ,--compiler CC ---------- use CC as the C compiler
+ -c,--compiler CC ---------- use CC as the C compiler
                              (default to CC env. variable or clang)
- -f,--file FILE ------------ instrumented to write trace to fILE
-                             (default to STDOUT)
  -F,--flags FLAGS ---------- comma-separated list of compiler flags
                              (default to CFLAGS env. variable)
  -o,--out-file FILE -------- write mutated source to FILE
                              (default STDOUT)
  -O,--orig ----------------- also save a copy of the original
  -q,--quiet ---------------- set verbosity level to 0
+ -t,--trace-file FILE ------ instrumented to write trace to fILE
+                             (default to STDERR)
  -v,--verbose NUM ---------- verbosity level 0-4
 
 Built with ~a version ~a.~%"
@@ -81,12 +128,12 @@ Built with ~a version ~a.~%"
 
     ;; Options.
     (getopts
-      (nil "--compiler" (setf (compiler original) (pop args)))
-      ("-f" "--file" (setf trace-file (pop args)))
+      ("-c" "--compiler" (setf (compiler original) (pop args)))
       ("-F" "--flags" (setf (flags original) (pop args)))
       ("-o" "--out-file" (setf out-file (pop args)))
       ("-O" "--orig" (setf save-original t))
       ("-q" "--quiet" (setf *note-level* 0))
+      ("-t" "--trace-file" (setf trace-file (pop args)))
       ("-v" "--verbose"   (let ((lvl (parse-integer (pop args))))
                             (when (>= lvl 4) (setf *shell-debug* t))
                             (setf *note-level* lvl))))
