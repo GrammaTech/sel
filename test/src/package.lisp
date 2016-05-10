4593e52a13d977b3bd849526819200c6e53b7524
Eric Schulte <eschulte@grammatech.com>
2016-05-10 13:14:07 -0400

    two test packaging fixes

--- test/src/package.lisp
+++ test/src/package.lisp
@@ -6,8 +6,11 @@
    :alexandria
    :metabang-bind
    :curry-compose-reader-macros
+   :cl-arrows
+   :iterate
    :stefil
    :cl-ppcre
    :software-evolution
    :software-evolution-utility)
+  (:shadowing-import-from :iterate :iter :for :until :collecting :in)
   (:export :test :batch-test))
