f563ca06012d04c5c1aba37a721a26d42e71d971
Eric Schulte <eschulte@grammatech.com>
2016-10-24 23:53:36 +0200

    initially adding view template for support

---
+++ view/package.lisp
@@ -0,0 +1,19 @@
+(defpackage :software-evolution-view
+  (:nicknames :se-view)
+  (:use
+   :common-lisp
+   :alexandria
+   :metabang-bind
+   :curry-compose-reader-macros
+   :iterate
+   :split-sequence
+   :trivial-shell
+   :cl-ppcre
+   :cl-store
+   :cl-dot
+   :diff)
+  (:export :view))
+
+#+allegro
+(set-dispatch-macro-character #\# #\_
+                              #'(lambda (s c n) (declare (ignore s c n)) nil))
