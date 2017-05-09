b7cea7a28f7a796ab8351a497e573ed6ac2d8eae
Eric Schulte <eschulte@grammatech.com>
2017-05-09 15:47:19 -0400

    Remove no-longer-necessary eval-when for ccrm
    The acronym ccrm is the curry-compose-reader-macros package which is now
    enabled itself via a macro which ensures it is evaluated when necessary.

--- test/src/software-evolution-test.lisp
+++ test/src/software-evolution-test.lisp
@@ -6,8 +6,7 @@
 
 ;;; Code:
 (in-package :software-evolution-test)
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (enable-curry-compose-reader-macros :include-utf8))
+(enable-curry-compose-reader-macros :include-utf8)
 
 ;; Disable clang-format and any other helpers
 (defmacro every-is (function &rest lists)
