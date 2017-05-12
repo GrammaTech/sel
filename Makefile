e0762976ae1d02150e121fa86489f9a077bb51e9
Eric Schulte <eschulte@grammatech.com>
2017-05-12 14:38:50 -0400

    Use external cl-make repo for Makefile boilerplate
    - Simplification to use external cl-make repo
    - Remember to add test executables to BINS
    - Bringing in fixes from master branch
    - Changes for BINS only holding the file names
    - BINS now only holds file names not paths
    - Updates from cl-make
    - Fix manifest for non-USER_QUICK_LISP builds

--- Makefile
+++ Makefile
@@ -1,145 +1,34 @@
-SHELL=bash
-
-.PHONY: testbot-check check doc clean
+.PHONY: doc
 
 # Set personal or machine-local flags in a file named local.mk
 ifneq ("$(wildcard local.mk)","")
 include local.mk
 endif
 
-# Use buildapp as the lisp compiler.
-LC ?= buildapp
-
-# You can set this as an environment variable to point to an alternate
-# quicklisp install location.  If you do, ensure that it ends in a "/"
-# character, and that you use the $HOME variable instead of ~.
-INSTDIR ?= /usr/synth
-QUICK_LISP ?= $(INSTDIR)/quicklisp/
-
-ifeq "$(wildcard $(QUICK_LISP)/setup.lisp)" ""
-$(warning $(QUICK_LISP) does not appear to be a valid quicklisp install)
-$(error Please point QUICK_LISP to your quicklisp installation)
-endif
-
-LISP_LIBS+= software-evolution
-LC_LIBS:=$(addprefix --load-system , $(LISP_LIBS))
-LOADED_LIBS:=$(addprefix quicklisp/local-projects/, $(LISP_LIBS:=.loaded))
-
+PACKAGE_NAME = software-evolution
+PACKAGE_NICKNAME = se
+PACKAGE_NAME_FIRST = software-evolution-utility
 LISP_DEPS =				\
 	$(wildcard *.lisp) 		\
-	$(wildcard software/*.lisp)	\
 	$(wildcard src/*.lisp)		\
+	$(wildcard software/*.lisp)	\
 	$(wildcard utility/*.lisp)
 
-# Flags to buildapp
-LCFLAGS=--manifest-file system-index.txt \
-	--asdf-tree quicklisp/dists/quicklisp/software
+TEST_ARTIFACTS = \
+	test/etc/gcd/gcd \
+	test/etc/gcd/gcd.s
 
-ifneq ($(LISP_STACK),)
-LCFLAGS+= --dynamic-space-size $(LISP_STACK)
-endif
+BINS = clang-instrument
 
-# Default lisp to build manifest file.
-LISP ?= ccl
-ifneq (,$(findstring sbcl, $(LISP)))
-ifeq ("$(SBCL_HOME)","")
-LISP_HOME = SBCL_HOME=$(dir $(shell which $(LISP)))../lib/sbcl
-endif
-endif
-ifneq (,$(findstring sbcl, $(LISP)))
-LISP_FLAGS = --no-userinit --no-sysinit
-else
-LISP_FLAGS = --quiet --no-init
-endif
+include cl.mk
 
-all: bin/clang-instrument
-
-system-index.txt: qlfile
-	$(LISP_HOME) $(LISP) $(LISP_FLAGS) --load $(QUICK_LISP)/setup.lisp \
-		--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
-		--eval '(ql:quickload :qlot)' \
-		--eval '(qlot:install :software-evolution)' \
-		--eval '(qlot:quickload :software-evolution)' \
-		--eval '(qlot:with-local-quicklisp (:software-evolution) (ql:register-local-projects))' \
-		--eval '#+sbcl (exit) #+ccl (quit)'
-
-quicklisp/local-projects/%.loaded: | system-index.txt
-	$(LISP_HOME) $(LISP) $(LISP_FLAGS) --load quicklisp/setup.lisp \
-		--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
-		--eval '(ql:quickload :$(notdir $*))' \
-		--eval "#+sbcl (exit) #+ccl (quit)"
-	touch $@
-
-bin/clang-instrument: $(LISP_DEPS) $(LOADED_LIBS) system-index.txt
-	CC=$(CC) $(LISP_HOME) LISP=$(LISP) $(LC) $(LCFLAGS) $(LC_LIBS) --output $@ --entry "se:clang-instrument"
-
-
-## Documentation
-doc:
-	make -C doc
-
-
-# Test executable
-TEST_LISP_DEPS=$(wildcard test/src/*.lisp)
-TEST_LISP_LIBS+= software-evolution-test
-TEST_LC_LIBS:=$(addprefix --load-system , $(TEST_LISP_LIBS))
-TEST_LOADED_LIBS:=$(addprefix quicklisp/local-projects/, $(TEST_LISP_LIBS:=.loaded))
-
-bin/se-test: $(TEST_LISP_DEPS) $(LISP_DEPS) $(TEST_LOADED_LIBS) system-index.txt
-	CC=$(CC) $(LISP_HOME) LISP=$(LISP) $(LC) $(LCFLAGS) $(TEST_LC_LIBS) --output $@ --entry "se-test:run-batch"
-
-bin/se-testbot-test: $(TEST_LISP_DEPS) $(LISP_DEPS) $(TEST_LOADED_LIBS) system-index.txt
-	CC=$(CC) $(LISP_HOME) LISP=$(LISP) $(LC) $(LCFLAGS) $(TEST_LC_LIBS) --output $@ --entry "se-test:run-testbot"
-
-
-## Testing
 test/etc/gcd/gcd: test/etc/gcd/gcd.c
 	$(CC) $< -o $@
 
 test/etc/gcd/gcd.s: test/etc/gcd/gcd.c
 	$(CC) $< -S -o $@
 
-TEST_ARTIFACTS = \
-	test/etc/gcd/gcd \
-	test/etc/gcd/gcd.s
-
-check: bin/se-test $(TEST_ARTIFACTS)
-	@$<
-
-testbot-check: bin/se-testbot-test $(TEST_ARTIFACTS)
-	@$<
-
 
-## Interactive testing
-SWANK_PORT ?= 4005
-swank: quicklisp/setup.lisp
-	$(LISP_HOME) $(LISP) $(LISP_FLAGS)			\
-	--load $<						\
-	--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
-	--eval '(ql:quickload :swank)'				\
-	--eval '(ql:quickload :software-evolution)'		\
-	--eval '(in-package :software-evolution)'		\
-	--eval '(swank:create-server :port $(SWANK_PORT) :style :spawn :dont-close t)'
-
-swank-test: quicklisp/setup.lisp $(TEST_ARTIFACTS)
-	$(LISP_HOME) $(LISP) $(LISP_FLAGS)			\
-	--load $<						\
-	--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
-	--eval '(ql:quickload :swank)'				\
-	--eval '(ql:quickload :software-evolution)'		\
-	--eval '(ql:quickload :software-evolution-test)'	\
-	--eval '(in-package :software-evolution-test)'		\
-	--eval '(swank:create-server :port $(SWANK_PORT) :style :spawn :dont-close t)'
-
-clean:
-	rm -f bin/se-test bin/se-testbot-test bin/clang-instrument
-	rm -f $(TEST_ARTIFACTS)
-
-more-clean: clean
-	find . -type f -name "*.fasl" -exec rm {} \+
-	find . -type f -name "*.lx32fsl" -exec rm {} \+
-
-real-clean: more-clean
-	find . -type f -name "*.loaded" -exec rm {} \+
-	rm -f qlfile.lock system-index.txt
-	rm -rf quicklisp
+## Documentation
+doc:
+	make -C doc
---
+++ cl.mk
@@ -0,0 +1,166 @@
+# To use this file first define the following variables in your
+# makefile and then include cl.mk.
+#
+# PACKAGE_NAME ------- The full name of the CL package
+# PACKAGE_NICKNAME --- The nickname of the CL package
+#                      (default: package name)
+# PACKAGE_NAME_FIRST - The first package name to require
+#                      (default: package name)
+# BINS --------------- Names of binaries to build with buildapp
+# TEST_ARTIFACTS ----- Name of dependencies for testing
+# LISP_DEPS ---------- Packages require to build CL package
+# TEST_LISP_DEPS ----- Packages require to build CL test package
+
+.PHONY: check-testbot check clean more-clean real-clean
+
+.SECONDARY:
+
+# Set default values of PACKAGE_NICKNAME/PACKAGE_NAME_FIRST
+PACKAGE_NICKNAME ?= $(PACKAGE_NAME)
+PICKAGE_NAME_FIRST ?= $(PACKAGE_NAME)
+
+# Use buildapp as the lisp compiler.
+LC ?= buildapp
+
+# You can set this as an environment variable to point to an alternate
+# quicklisp install location.  If you do, ensure that it ends in a "/"
+# character, and that you use the $HOME variable instead of ~.
+INSTDIR ?= /usr/synth
+QUICK_LISP ?= $(INSTDIR)/quicklisp/
+
+# This variable allows users to do everything using their own
+# quicklisp directory instead of using qlot.
+ifeq ($(USER_QUICK_LISP),)
+USER_QUICK_LISP=quicklisp
+MANIFEST=system-index.txt
+else
+MANIFEST=$(USER_QUICK_LISP)/local-projects/system-index.txt
+endif
+
+ifeq "$(wildcard $(QUICK_LISP)/setup.lisp)" ""
+$(warning $(QUICK_LISP) does not appear to be a valid quicklisp install)
+$(error Please point QUICK_LISP to your quicklisp installation)
+endif
+
+LISP_LIBS += $(PACKAGE_NAME)
+LC_LIBS:=$(addprefix --load-system , $(LISP_LIBS))
+LOADED_LIBS:=$(addprefix $(USER_QUICK_LISP)/local-projects/, $(LISP_LIBS:=.loaded))
+
+LISP_DEPS ?=				\
+	$(wildcard *.lisp) 		\
+	$(wildcard src/*.lisp)
+
+# Flags to buildapp
+LCFLAGS=--manifest-file $(MANIFEST) \
+	--asdf-tree $(USER_QUICK_LISP)/dists/quicklisp/software
+
+ifneq ($(LISP_STACK),)
+LCFLAGS+= --dynamic-space-size $(LISP_STACK)
+endif
+
+# Default lisp to build manifest file.
+LISP ?= ccl
+ifneq (,$(findstring sbcl, $(LISP)))
+ifeq ("$(SBCL_HOME)","")
+LISP_HOME = SBCL_HOME=$(dir $(shell which $(LISP)))../lib/sbcl
+endif
+endif
+ifneq (,$(findstring sbcl, $(LISP)))
+LISP_FLAGS = --no-userinit --no-sysinit
+else
+LISP_FLAGS = --quiet --no-init
+endif
+
+all: $(addprefix bin/, $(BINS))
+
+# In this target we require :$(PACKAGE_NAME_FIRST) instead of
+# :$(PACKAGE_NAME) because the later may depend on the former causing
+# an error if :$(PACKAGE_NAME_FIRST) is not found.
+PACKAGE_NAME_FIRST ?= $(PACKAGE_NAME)
+ifeq ($(USER_QUICK_LISP),quicklisp)
+$(MANIFEST): qlfile
+	$(LISP_HOME) $(LISP) $(LISP_FLAGS) --load $(QUICK_LISP)/setup.lisp \
+		--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
+		--eval '(ql:quickload :qlot)' \
+		--eval '(qlot:install :$(PACKAGE_NAME_FIRST))' \
+		--eval '(qlot:quickload :$(PACKAGE_NAME_FIRST))' \
+		--eval '(qlot:with-local-quicklisp (:$(PACKAGE_NAME_FIRST)) (ql:register-local-projects))' \
+		--eval '#+sbcl (exit) #+ccl (quit)'
+else
+$(MANIFEST):
+	$(LISP_HOME) $(LISP) $(LISP_FLAGS) --load $(USER_QUICK_LISP)/setup.lisp \
+		--eval '(ql:register-local-projects)' \
+		--eval '#+sbcl (exit) #+ccl (quit)'
+endif
+
+$(USER_QUICK_LISP)/local-projects/%.loaded: | $(MANIFEST)
+	$(LISP_HOME) $(LISP) $(LISP_FLAGS) --load $(USER_QUICK_LISP)/setup.lisp \
+		--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
+		--eval '(ql:quickload :$(notdir $*))' \
+		--eval "#+sbcl (exit) #+ccl (quit)"
+	touch $@
+
+bin/%: $(LISP_DEPS) $(LOADED_LIBS) $(MANIFEST)
+	CC=$(CC) $(LISP_HOME) LISP=$(LISP) $(LC) $(LCFLAGS) $(LC_LIBS) --output $@ --entry "$(PACKAGE_NICKNAME):$*"
+
+
+# Test executable
+BINS += $(PACKAGE_NICKNAME)-test
+BINS += $(PACKAGE_NICKNAME)-testbot
+
+TEST_LISP_DEPS ?= $(wildcard test/src/*.lisp)
+TEST_LISP_LIBS += $(PACKAGE_NAME)-test
+TEST_LC_LIBS:=$(addprefix --load-system , $(TEST_LISP_LIBS))
+TEST_LOADED_LIBS:=$(addprefix $(USER_QUICK_LISP)/local-projects/, $(TEST_LISP_LIBS:=.loaded))
+
+bin/$(PACKAGE_NICKNAME)-test: $(TEST_LISP_DEPS) $(LISP_DEPS) $(TEST_LOADED_LIBS) $(MANIFEST)
+	CC=$(CC) $(LISP_HOME) LISP=$(LISP) $(LC) $(LCFLAGS) $(TEST_LC_LIBS) --output $@ --entry "$(PACKAGE_NICKNAME)-test:run-batch"
+
+bin/$(PACKAGE_NICKNAME)-testbot: $(TEST_LISP_DEPS) $(LISP_DEPS) $(TEST_LOADED_LIBS) $(MANIFEST)
+	CC=$(CC) $(LISP_HOME) LISP=$(LISP) $(LC) $(LCFLAGS) $(TEST_LC_LIBS) --output $@ --entry "$(PACKAGE_NICKNAME)-test:run-testbot"
+
+
+## Testing
+TEST_ARTIFACTS ?=
+
+check: bin/$(PACKAGE_NICKNAME)-test $(TEST_ARTIFACTS)
+	@$<
+
+check-testbot: bin/$(PACKAGE_NICKNAME)-testbot $(TEST_ARTIFACTS)
+	@$<
+
+
+## Interactive testing
+SWANK_PORT ?= 4005
+swank: $(USER_QUICK_LISP)/setup.lisp
+	$(LISP_HOME) $(LISP) $(LISP_FLAGS)			\
+	--load $<						\
+	--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
+	--eval '(ql:quickload :swank)'				\
+	--eval '(ql:quickload :$(PACKAGE_NAME))'		\
+	--eval '(in-package :$(PACKAGE_NAME))'			\
+	--eval '(swank:create-server :port $(SWANK_PORT) :style :spawn :dont-close t)'
+
+swank-test: $(USER_QUICK_LISP)/setup.lisp $(TEST_ARTIFACTS)
+	$(LISP_HOME) $(LISP) $(LISP_FLAGS)			\
+	--load $<						\
+	--eval '(pushnew (truename ".") ql:*local-project-directories*)' \
+	--eval '(ql:quickload :swank)'				\
+	--eval '(ql:quickload :$(PACKAGE_NAME))'		\
+	--eval '(ql:quickload :$(PACKAGE_NAME)-test)'		\
+	--eval '(in-package :$(PACKAGE_NAME)-test)'		\
+	--eval '(swank:create-server :port $(SWANK_PORT) :style :spawn :dont-close t)'
+
+clean:
+	rm -f bin/$(PACKAGE_NICKNAME)-test bin/$(PACKAGE_NICKNAME)-testbot $(addprefix bin/, $(BINS))
+	rm -f $(TEST_ARTIFACTS)
+
+more-clean: clean
+	find . -type f -name "*.fasl" -exec rm {} \+
+	find . -type f -name "*.lx32fsl" -exec rm {} \+
+	find . -type f -name "*.lx64fsl" -exec rm {} \+
+
+real-clean: more-clean
+	find . -type f -name "*.loaded" -exec rm {} \+
+	rm -f qlfile.lock $(MANIFEST)
+	rm -rf quicklisp
