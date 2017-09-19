a330f716fd340b211b694ea3a6f5e89e73960837
Jason Ruchti <jruchti@grammatech.com>
2017-09-19 12:29:06 -0400

    Ensure -I flags point to a valid directory.
    When creating a clang project, we build the project
    within a temporary directory.  As such, -I flags
    generally point to directories within this temp area
    which is destroyed after the build.
    
    To ensure proper operation with clang-mutate after
    the temp area has been destroyed, this commit replaces
    the -I flags pointing the temporary build area with
    the analogous persistent project directory.

--- software/clang-project.lisp
+++ software/clang-project.lisp
@@ -44,24 +44,47 @@
                            (build-command clang-project)
                            (build-target clang-project)
                            stdout stderr))))
+           (relativize (clang-project path)
+             (replace-all (-> (if (-> (cl-fad:pathname-as-directory path)
+                                      (cl-fad:directory-exists-p))
+                                  (cl-fad:pathname-as-directory path)
+                                  path)
+                              (cl-fad:canonical-pathname)
+                              (namestring))
+                          (-> (project-dir clang-project)
+                              (cl-fad:pathname-as-directory)
+                              (cl-fad:canonical-pathname)
+                              (namestring))
+                          ""))
+           (get-project-path (clang-project path)
+             (replace-all (-> (cl-fad:canonical-pathname path)
+                              (namestring))
+                          (-> (if (and *build-dir*
+                                       (search (-> *build-dir*
+                                                   (namestring))
+                                               (-> path
+                                                   (cl-fad:canonical-pathname)
+                                                   (cl-fad:pathname-as-directory)
+                                                   (namestring))))
+                                  *build-dir*
+                                  (project-dir clang-project))
+                              (cl-fad:canonical-pathname)
+                              (cl-fad:pathname-as-directory)
+                              (namestring))
+                          (-> (project-dir clang-project)
+                              (cl-fad:canonical-pathname)
+                              (cl-fad:pathname-as-directory)
+                              (namestring))))
            (create-evolve-files (clang-project)
              (iter (for entry in (compilation-database clang-project))
                    (collect
-                     (cons (replace-all (aget :file entry)
-                                        (-> (if (and *build-dir*
-                                                     (search
-                                                       (namestring *build-dir*)
-                                                       (aget :file entry)))
-                                                *build-dir*
-                                                (project-dir clang-project))
-                                            (cl-fad:pathname-as-directory)
-                                            (namestring))
-                                        "")
-                           (from-file (make-instance
-                                        (clang-class clang-project)
-                                        :compiler (get-compiler entry)
-                                        :flags (get-flags entry))
-                                      (aget :file entry))))))
+                     (cons (->> (get-project-path clang-project
+                                                  (aget :file entry))
+                                (relativize clang-project))
+                           (->  (make-instance (clang-class clang-project)
+                                               :compiler (get-compiler entry)
+                                               :flags (get-flags entry))
+                                (from-file (aget :file entry)))))))
            (get-compiler (compilation-database-entry)
              (->> (aget :command compilation-database-entry)
                   (split-sequence #\Space)
@@ -70,26 +93,29 @@
              (let ((flags (-<>> (aget :command compilation-database-entry)
                                 (replace-all <> "-I" "-I ")
                                 (split-sequence #\Space)
-                                (remove-if {string= ""}))))
-               (->> (iter (for (prev . f) in (mapcar #'cons
-                                                     flags
-                                                     (cdr flags)))
+                                (remove-if {string= ""})
+                                (cdr))))
+               (->> (iter (for f in flags)
+                          (for p previous f)
                           (collect
-                            (if (string= prev "-I")
+                            (if (string= p "-I")
                                 (if (starts-with-subseq "/" f)
-                                    f
-                                    (-> (cl-fad:merge-pathnames-as-directory
-                                          (make-pathname :directory f)
-                                          (->> (aget :directory
-                                                     compilation-database-entry)
-                                               (make-pathname :directory)))
-                                        (directory-namestring)))
+                                    (->> (cl-fad:pathname-as-directory f)
+                                         (get-project-path clang-project))
+                                    (->> (cl-fad:merge-pathnames-as-directory
+                                           (->> (aget :directory
+                                                      compilation-database-entry)
+                                                (make-pathname :directory))
+                                           (make-pathname :directory f))
+                                         (cl-fad:pathname-as-directory)
+                                         (get-project-path clang-project)))
                                 f)))
                     (remove-if {string= (aget :file compilation-database-entry)})
                     (append (list "-I" (-> (project-dir clang-project)
                                            (namestring))))))))
     (setf (project-dir clang-project)
-          (truename project-dir))
+          (-> (truename project-dir)
+              (cl-fad:canonical-pathname)))
     (cond ((all-files clang-project)
            ;; reload existing files
            (iter (for (src-file . obj) in (all-files clang-project))
