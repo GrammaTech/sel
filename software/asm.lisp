1a925775c6fc3b3f4bb3ef8ed38ddbf0b0622dee
Rebecca Swords <rswords@grammatech.com>
2018-01-25 15:36:45 -0600

    Replace DOCFIXMEs with documentation in asm.lisp

--- software/asm.lisp
+++ software/asm.lisp
@@ -28,11 +28,15 @@
 mutations (simple plus asm mutations).")
 
 (defmethod pick-mutation-type ((asm asm))
-  "DOCFIXME"
+  "Return a symbol naming a randomly selected ASM mutation."
   (random-pick *asm-mutation-types*))
 
 (defmethod phenome ((asm asm) &key (bin (temp-file-name)))
-  "DOCFIXME"
+  "Runs the `linker' for ASM, using the specified `flags' for ASM and returns
+multiple values holding in order: (1) the binary path BIN to which the
+executable was compiled, (2) the errno, or a numeric indication of success, of
+the linking process, (3) STDERR of the linking process, (4) STDOUT of the
+linking process, (5) the source file name used during linking."
   #-ccl (declare (values t fixnum string string string))
   (with-temp-file-of (src "s") (genome-string asm)
     (multiple-value-bind (stdout stderr errno)
@@ -74,7 +78,11 @@ with an operand in the second."))
   (assoc-value (elt (genome asm) n) :code))
 
 (defmethod apply-mutation ((asm asm) (mutation asm-replace-operand))
-  "DOCFIXME"
+  "Apply an asm-replace-operand MUTATION to ASM, return the resulting software.
+The MUTATION targets are a pair of instruction indices pointing to a \"bad\"
+instruction (whose operand will be replaced) and a \"good\" instruction
+(whose operand will be used as the replacement). If either instruction lacks an
+operand, a `no-mutation-targets' condition is raised."
   (let ((bad-good (targets mutation)))
     (assert (listp bad-good) (mutation)
             "Requires mutations targets to be a list of two elements.")
