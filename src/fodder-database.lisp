c83ff877c14f9622b63312c994c27c15255131b5
Amy Gale <amyg@FIFE@grammatech.org>
2018-01-31 12:48:49 -0500

    Add "Searchability" Section to API Docs
    Searchability section covers classes 'searchable', 'fodder-database', 'in-memory-database', 'json-database', 'pliny-database'.
    
    Also add 'adaptive-mutation' coverage to API | Software objects section.
    
    Split new file doc/api--searchability.texi out of doc/sel-api.texi.
    
    Regularize manual "Copying" section structure.

--- src/fodder-database.lisp
+++ src/fodder-database.lisp
@@ -13,11 +13,11 @@
 (defgeneric find-snippets (database &key ast-class full-stmt decls limit)
   (:documentation "Find snippets in the fodder database DATABASE.
 
-:AST-CLASS - AST class all snippets should match
-:FULL-STMT - Limit results to full statements if non-nil.
-:DECLS ----- Include decls in result if non-nil.
-             Limit results to decls if the keyword :ONLY.
-:LIMIT ----- Limit to N randomly drawn snippets"))
+* AST-CLASS   AST class all snippets should match
+* FULL-STMT   Limit results to full statements if non-nil.
+* DECLS       Include decls in result if non-nil.
+              Limit results to decls if the keyword :ONLY.
+* LIMIT       Limit to N randomly drawn snippets"))
 
 (defgeneric find-type (database hash)
   (:documentation "Find the type in the database
