88246fc24f0775d6175712de49d69a753fe5fabe
Jason Ruchti <jruchti@grammatech.com>
2018-03-25 21:48:17 -0400

    Adding license pre/postfixes.

--- src/clang-tokens.lisp
+++ src/clang-tokens.lisp
@@ -1,3 +1,43 @@
+#||
+*******************************************************************************
+*
+* This software is furnished under a license and/or other restrictive terms and may
+* be used and copied only in accordance with such terms and the inclusion of the
+* below copyright notice. This software or any other copies thereof may not be
+* provided or otherwise made available to any other person without the express
+* written consent of an authorized reprsentative of GrammaTech, Inc. Title to,
+* ownership of, and all rights in the software is retained by GrammaTech, Inc.
+*
+* Any reproduction of the software or portions thereof marked with this legend
+* must also reproduce the following markings.
+*
+* Unless otherwise specified, software artifacts in this directory and its
+* subdirectories are subject to:
+*
+* Distribution Statement B: Distribution authorized to U.S. Government agencies only;
+* Export Controlled and Proprietary Information: DFARS SBIR Data Rights, 09/15/2017.
+* Other requests for this document shall be referred to the DoD Controlling
+* Office or the DoD SBIR Program Office.
+*
+* SBIR Data Rights
+*
+* Contract Number: D17PC00096
+* Contractor Name: GrammaTech, Inc.
+* Contractor Address: 531 Esty Street, Ithaca, NY  14850
+* Expiration Date: April 30, 2024
+*
+* The Governmentís rights to use, modify, reproduce, release, perform, display, or disclose
+* technical data or computer software contained in this report are restricted by paragraph
+* (b)(4) of the Rights in Noncommercial Technical Data and Computer Software - Small Business
+* Innovation Research (SBIR) Program clause (DFARS 252.227-7018 (FEB 2014)) contained in the
+* above identified contract. No restrictions apply after the expiration date shown above. Any
+* reproduction of technical data, computer software, or portions thereof marked with this
+* legend must also reproduce the markings.
+*
+* (c) GrammaTech, Inc. 2018.  All other rights reserved.
+*
+*******************************************************************************
+||#
 (in-package :software-evolution-library)
 (in-readtable :curry-compose-reader-macros)
 
@@ -375,3 +415,44 @@ SOFTWARE."))
                    :text (format nil "Unrecognized AST class ~a"
                                  (ast-class root)))))))))
     (tokenize-children roots)))
+
+#||
+*******************************************************************************
+*
+* This software is furnished under a license and/or other restrictive terms and may
+* be used and copied only in accordance with such terms and the inclusion of the
+* below copyright notice. This software or any other copies thereof may not be
+* provided or otherwise made available to any other person without the express
+* written consent of an authorized reprsentative of GrammaTech, Inc. Title to,
+* ownership of, and all rights in the software is retained by GrammaTech, Inc.
+*
+* Any reproduction of the software or portions thereof marked with this legend
+* must also reproduce the following markings.
+*
+* Unless otherwise specified, software artifacts in this directory and its
+* subdirectories are subject to:
+*
+* Distribution Statement B: Distribution authorized to U.S. Government agencies only;
+* Export Controlled and Proprietary Information: DFARS SBIR Data Rights, 09/15/2017.
+* Other requests for this document shall be referred to the DoD Controlling
+* Office or the DoD SBIR Program Office.
+*
+* SBIR Data Rights
+*
+* Contract Number: D17PC00096
+* Contractor Name: GrammaTech, Inc.
+* Contractor Address: 531 Esty Street, Ithaca, NY  14850
+* Expiration Date: April 30, 2024
+*
+* The Governmentís rights to use, modify, reproduce, release, perform, display, or disclose
+* technical data or computer software contained in this report are restricted by paragraph
+* (b)(4) of the Rights in Noncommercial Technical Data and Computer Software - Small Business
+* Innovation Research (SBIR) Program clause (DFARS 252.227-7018 (FEB 2014)) contained in the
+* above identified contract. No restrictions apply after the expiration date shown above. Any
+* reproduction of technical data, computer software, or portions thereof marked with this
+* legend must also reproduce the markings.
+*
+* (c) GrammaTech, Inc. 2018.  All other rights reserved.
+*
+*******************************************************************************
+||#
--- test/src/testbot.lisp
+++ test/src/testbot.lisp
@@ -1,3 +1,43 @@
+#||
+*******************************************************************************
+*
+* This software is furnished under a license and/or other restrictive terms and may
+* be used and copied only in accordance with such terms and the inclusion of the
+* below copyright notice. This software or any other copies thereof may not be
+* provided or otherwise made available to any other person without the express
+* written consent of an authorized reprsentative of GrammaTech, Inc. Title to,
+* ownership of, and all rights in the software is retained by GrammaTech, Inc.
+*
+* Any reproduction of the software or portions thereof marked with this legend
+* must also reproduce the following markings.
+*
+* Unless otherwise specified, software artifacts in this directory and its
+* subdirectories are subject to:
+*
+* Distribution Statement B: Distribution authorized to U.S. Government agencies only;
+* Export Controlled and Proprietary Information: DFARS SBIR Data Rights, 09/15/2017.
+* Other requests for this document shall be referred to the DoD Controlling
+* Office or the DoD SBIR Program Office.
+*
+* SBIR Data Rights
+*
+* Contract Number: D17PC00096
+* Contractor Name: GrammaTech, Inc.
+* Contractor Address: 531 Esty Street, Ithaca, NY  14850
+* Expiration Date: April 30, 2024
+*
+* The Governmentís rights to use, modify, reproduce, release, perform, display, or disclose
+* technical data or computer software contained in this report are restricted by paragraph
+* (b)(4) of the Rights in Noncommercial Technical Data and Computer Software - Small Business
+* Innovation Research (SBIR) Program clause (DFARS 252.227-7018 (FEB 2014)) contained in the
+* above identified contract. No restrictions apply after the expiration date shown above. Any
+* reproduction of technical data, computer software, or portions thereof marked with this
+* legend must also reproduce the markings.
+*
+* (c) GrammaTech, Inc. 2018.  All other rights reserved.
+*
+*******************************************************************************
+||#
 ;;; External replacement for GT-specific test submission helpers
 (defun batch-test (test &optional args)
   "Run tests in 'batch' mode, printing results as a string."
@@ -21,3 +61,44 @@
 (defun testbot-test (test project branch &optional args)
   (declare (ignorable test project branch args))
   (error "Function `testbot-test' only makes sense inside GrammaTech."))
+
+#||
+*******************************************************************************
+*
+* This software is furnished under a license and/or other restrictive terms and may
+* be used and copied only in accordance with such terms and the inclusion of the
+* below copyright notice. This software or any other copies thereof may not be
+* provided or otherwise made available to any other person without the express
+* written consent of an authorized reprsentative of GrammaTech, Inc. Title to,
+* ownership of, and all rights in the software is retained by GrammaTech, Inc.
+*
+* Any reproduction of the software or portions thereof marked with this legend
+* must also reproduce the following markings.
+*
+* Unless otherwise specified, software artifacts in this directory and its
+* subdirectories are subject to:
+*
+* Distribution Statement B: Distribution authorized to U.S. Government agencies only;
+* Export Controlled and Proprietary Information: DFARS SBIR Data Rights, 09/15/2017.
+* Other requests for this document shall be referred to the DoD Controlling
+* Office or the DoD SBIR Program Office.
+*
+* SBIR Data Rights
+*
+* Contract Number: D17PC00096
+* Contractor Name: GrammaTech, Inc.
+* Contractor Address: 531 Esty Street, Ithaca, NY  14850
+* Expiration Date: April 30, 2024
+*
+* The Governmentís rights to use, modify, reproduce, release, perform, display, or disclose
+* technical data or computer software contained in this report are restricted by paragraph
+* (b)(4) of the Rights in Noncommercial Technical Data and Computer Software - Small Business
+* Innovation Research (SBIR) Program clause (DFARS 252.227-7018 (FEB 2014)) contained in the
+* above identified contract. No restrictions apply after the expiration date shown above. Any
+* reproduction of technical data, computer software, or portions thereof marked with this
+* legend must also reproduce the markings.
+*
+* (c) GrammaTech, Inc. 2018.  All other rights reserved.
+*
+*******************************************************************************
+||#
