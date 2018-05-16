dcd76b01ea14b4a5de62ba9f0ab6652f9dc81995
Eric Schulte <eschulte@grammatech.com>
2018-05-16 14:38:20 -0400

    Update ast-diff-to-html to match new ast-diff int.

--- ast-diff/ast-diff-html.lisp
+++ ast-diff/ast-diff-html.lisp
@@ -1,11 +1,11 @@
 ;;; ast-diff-html.lisp --- render ast diffs to html
 (defpackage :software-evolution-library/ast-diff-html
   (:nicknames :sel/ast-diff-html)
-  (:use :common-lisp :cl-who)
+  (:use :common-lisp :sel/utility :sel/ast-diff :cl-who)
   (:export :diff-to-html))
 (in-package :software-evolution-library/ast-diff-html)
 
-(defun diff-to-html (interface orig-asts edit-script)
+(defun diff-to-html (orig-asts edit-script &optional (stream t))
   "Generate HTML which shows side-by-side diff.
 
 Shows source text of ORIG-ASTS alongside the result of applying
@@ -13,50 +13,48 @@ EDIT-SCRIPT, with highlighting of inserts and deletes.
 "
   (labels
       ((render-ast (ast)
-         (let ((text (sel/ast-diff::ast-text interface ast)))
-           (values (sel/ast-diff::escape-string text) (count #\newline text))))
+         (let ((text (ast-text ast)))
+           (values (escape-string text) (count #\newline text))))
        (render-diff (asts script)
          (if (null script)
              '(nil nil)
-             (destructuring-bind (action . args) (car script)
-               (ecase action
-                 (:recurse
-                  (mapcar #'append
-                          (render-diff (cdr (car asts)) args)
-                          (render-diff (cdr asts) (cdr script))))
-                 (:same
-                  (assert (apply #'sel/ast-diff::ast-equal-p interface args))
+           (destructuring-bind (action . args) (car script)
+             (ecase action
+               (:recurse
+                (mapcar #'append
+                        (render-diff (cdr (car asts)) args)
+                        (render-diff (cdr asts) (cdr script))))
+               (:same
+                (mapcar #'cons
+                        (make-list 2
+                                   :initial-element
+                                   (render-ast (car asts)))
+                        (render-diff (cdr asts) (cdr script))))
+               (:delete
+                (multiple-value-bind (text line-count)
+                    (render-ast (car asts))
                   (mapcar #'cons
-                          (make-list 2
-                                     :initial-element
-                                     (render-ast (car asts)))
-                          (render-diff (cdr asts) (cdr script))))
-                 (:delete
-                  (assert (sel/ast-diff::ast-equal-p interface (car asts) args))
-                  (multiple-value-bind (text line-count)
-                      (render-ast (car asts))
-                    (mapcar #'cons
-                            (list (format nil
-                                          "<span class=\"delete\">~a</span>"
-                                          text)
-                                  (format nil "~{~a~}"
-                                          (make-list line-count
-                                                     :initial-element
-                                                     #\newline)))
-                            (render-diff (cdr asts) (cdr script)))))
-                 (:insert
-                  (multiple-value-bind (text line-count)
-                      (render-ast args)
-                    (mapcar #'cons
-                            (list (format nil "~{~a~}"
-                                          (make-list line-count
-                                                     :initial-element
-                                                     #\newline))
-                                  (format nil
-                                          "<span class=\"insert\">~a</span>"
-                                          text))
-                            (render-diff asts (cdr script))))))))))
-    (apply #'format nil "<!DOCTYPE html>
+                          (list (format nil
+                                        "<span class=\"delete\">~a</span>"
+                                        text)
+                                (format nil "~{~a~}"
+                                        (make-list line-count
+                                                   :initial-element
+                                                   #\newline)))
+                          (render-diff (cdr asts) (cdr script)))))
+               (:insert
+                (multiple-value-bind (text line-count)
+                    (render-ast args)
+                  (mapcar #'cons
+                          (list (format nil "~{~a~}"
+                                        (make-list line-count
+                                                   :initial-element
+                                                   #\newline))
+                                (format nil
+                                        "<span class=\"insert\">~a</span>"
+                                        text))
+                          (render-diff asts (cdr script))))))))))
+    (apply #'format stream "<!DOCTYPE html>
 <html>
   <head>
     <style>
@@ -91,4 +89,4 @@ EDIT-SCRIPT, with highlighting of inserts and deletes.
 <div class=\"column\"> <pre class=\"pre\">~{~a~}</pre></div>
 </body>
 </html>"
-           (render-diff (cdr orig-asts) edit-script))))
+           (traced (render-diff orig-asts edit-script)))))
