2e106fb5bf2515ae339ee6d682f229a840e24e94
Roger Corman <roger@corman.net>
2018-06-11 21:01:51 -0400

    Decreased timeout to .01 seconds.

--- software/asm-super-mutant-fitness.c
+++ software/asm-super-mutant-fitness.c
@@ -363,8 +363,8 @@ static struct sigaction tsa;
 static sigset_t mask;
 
 void start_timer() {
-    timer.it_value.tv_nsec = 0;
-    timer.it_value.tv_sec = 1;
+    timer.it_value.tv_nsec = 10000000;  // .01 second timeout
+    timer.it_value.tv_sec = 0;
     timer.it_interval.tv_sec = 0;
     timer.it_interval.tv_nsec = 0;
     if (timer_settime(timerid, 0, &timer, NULL) == -1) {
