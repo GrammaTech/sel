111092beb4655e994c165443810bc1be8b2ba294
Rebecca Swords <rswords@grammatech.com>
2018-08-09 09:23:20 -0500

    Fix two csurf-asm bugs
    1. Remove file-existence requirement when parsing names of linked
    libraries from log file. Files that don't exist are handled later. The
    linked-files field should list all linked library names.
    2. Ensure there's a space when using "-l :libname" flag in linker
    command.

--- software/csurf-asm.lisp
+++ software/csurf-asm.lisp
@@ -112,7 +112,7 @@ set."
                            (collecting
                             (subseq (pathname-file file) 3))
                            ;; non-standard name, use :name
-                           (collecting (format nil ":~a"
+                           (collecting (format nil " :~a"
                                                (file-namestring file)))))))))
     (with-temp-file-of (src "s") (genome-string asm)
       (with-temp-file (obj)
@@ -194,7 +194,6 @@ Currently only populates the `weak-symbols' field from the log."
                  ;; Current arg is a path: it starts with /, is a file, and
                  ;; isn't a flag param (i.e., prev-str isn't a single dash flag)
                  (when (and (starts-with-subseq "/" str)
-                            (file-exists-p str)
                             (not (equal "--dynamic-linker" prev-str))
                             (or (starts-with-subseq "--" prev-str)
                                 (not (starts-with-subseq "-" prev-str))))
