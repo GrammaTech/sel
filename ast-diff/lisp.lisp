c3ef246c4f84c81f808ed2a4676233ac467e87b1
Eric Schulte <eschulte@grammatech.com>
2018-11-03 19:28:17 -0400

    Bring in changes from branch using Eclector branch
    This [1] branch of SEL makes use of changes from the [2] branch of
    Eclector package.  With this functionality it is possible to parse a
    lisp source file including all comments and whitespace into a concrete
    syntax tree which may be manipulated and then re-printed to fully
    reproduce the original source.
    
    [1] wip-lisp-diff-uses-eclector-parse-result-protocol-2
    [2] wip-parse-result-protocol-2

--- ast-diff/lisp.lisp
+++ ast-diff/lisp.lisp
@@ -35,143 +35,169 @@
         :uiop
         :software-evolution-library/utility
         :software-evolution-library/ast-diff
-        :eclector.concrete-syntax-tree)
+        :eclector.parse-result)
   (:shadowing-import-from
    :uiop :getenv :directory-exists-p :copy-file :appendf :parse-body
    :ensure-list :simple-style-warning :ensure-gethash :ensure-function
    :if-let :emptyp :featurep)
-  (:shadowing-import-from :concrete-syntax-tree
-                          :first :rest :source :cst :cons-cst :atom-cst :raw)
-  (:export :run-lisp-diff))
+  (:shadowing-import-from :eclector.parse-result :read)
+  (:shadowing-import-from :software-evolution-library/view
+                          +color-RED+ +color-GRN+ +color-RST+))
 (in-package :software-evolution-library/ast-diff/lisp)
 (in-readtable :curry-compose-reader-macros)
 
 
-;;; Build up a concrete syntax tree which includes comment regions.
-(defvar *comments*)
+;;; Differencing adapted from second-climacs-test.lisp in
+;;; wip-parse-result-protocol-2 branch of eclector.
 (defvar *string*)
 
-(defclass comment ()
-  ((kind :initform nil :initarg :kind :accessor kind)
-   (source :initform nil :initarg :source :accessor source)
-   (raw :initform nil :initarg :raw :accessor raw)))
+(defclass result ()
+  ((start :initarg :start
+          :reader  start)
+   (end   :initarg :end
+          :reader  end)
+   (string-pointer :initarg :string-pointer
+                   :initform *string*
+                   :reader  string-pointer)))
 
-(defmethod print-object ((object comment) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "raw: ~s" (raw object))))
+(defclass expression-result (result)
+  ((expression :initarg :expression
+               :reader  expression)
+   (children   :initarg :children
+               :accessor children)))
 
-(defclass whitespace ()
-  ((source :initform nil :initarg :source :accessor source)
-   (raw :initform nil :initarg :raw :accessor raw)))
+(defclass skipped-input-result (result)
+  ((reason :initarg :reason
+           :reader  reason)))
 
-(defmethod print-object ((object whitespace) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "source: ~s" (source object))))
+(defclass second-climacs (parse-result-mixin)
+  ((cache :reader   cache
+          :initform (make-hash-table :test #'eql))))
 
-(defmethod record-skipped-input (client stream reason source)
-  (declare (ignorable client stream))
-  (push (make-instance 'comment
-          :kind reason
-          :source source
-          :raw (subseq *string* (car source) (cdr source)))
-        *comments*))
+(defmethod getcache ((position t) (client second-climacs))
+  (gethash position (cache client)))
 
-(defun read-file-forms-and-extras (file &key count)
+(defmethod (setf getcache) ((new-value t) (position t) (client second-climacs))
+  (setf (gethash position (cache client)) new-value))
+
+(defmethod make-expression-result
+    ((client second-climacs) (result t) (children t) (source cons))
+  (make-instance 'expression-result
+    :expression result
+    :children children
+    :start (car source)
+    :end (cdr source)))
+
+(defmethod make-skipped-input-result
+    ((client second-climacs) stream reason source)
+  (make-instance 'skipped-input-result
+    :reason reason :start (car source) :end (cdr source)))
+
+(defmethod eclector.reader:read-common :around
+    ((client second-climacs) input-stream eof-error-p eof-value)
+  (let ((position (source-position client input-stream)))
+    (if-let ((cached (getcache position client)))
+      (progn
+        (assert (eql (start cached) position))
+        (loop :repeat (- (end cached) position) :do (read-char input-stream))
+        (values (expression cached) cached))
+      (progn
+        (multiple-value-bind (expression parse-result) (call-next-method)
+          (setf (getcache position client) parse-result)
+          (values expression parse-result))))))
+
+(defun read-file-forms+ (file &key count)
   "Read forms and comments from FILE returned interleaved in a tree."
   (check-type count (or null integer))
-  (let ((previous 0) *comments* holding)
-    (labels ((starts (object)
-               (etypecase object
-                 ((or atom-cst comment) (car (source object)))
-                 (cons (or (starts (car object))
-                           (starts (cdr object))))))
-             (ends (object)
-               (etypecase object
-                 ((or atom-cst comment) (cdr (source object)))
-                 (cons (or (ends (car object))
-                           (ends (cdr object))))))
-             (cst-to-cons (object)
-               (if (concrete-syntax-tree:consp object)
-                   (progn
-                     (unless (first object)
-                       (setf (source (first object))
-                             (source (rest object))))
-                     (cons (cst-to-cons (first object))
-                           (cst-to-cons (rest object))))
-                   object))
-             (thread (csts)
-               ;; Thread *COMMENTS* through CSTS.
-               (if (null *comments*)
-                   csts
-                   (when csts
-                     (if (consp csts)
-                         (if (< (starts *comments*) (starts csts))
-                             (cons (pop *comments*) (thread csts))
-                             (if (starts (car csts))
-                                 (cons (thread (car csts))
-                                       (thread (cdr csts)))
-                                 (progn (push (car csts) holding)
-                                        (thread (cdr csts)))))
-                         (if (and (starts csts)
-                                  (< (starts *comments*) (starts csts)))
-                             (cons (pop *comments*) csts)
-                             (let ((ret (if (null (raw csts)) nil csts)))
-                               (if holding
-                                   (cons (pop holding) ret)
-                                   ret)))))))
-             (add-whitespace (tree)
-               (when tree
-                 (let ((end (starts tree)))
-                   (if (< previous end)
-                       (cons (make-instance 'whitespace
-                               :raw (subseq *string* previous end)
-                               :source (cons previous end))
-                             (progn (setf previous (ends tree))
-                                    (add-whitespace tree)))
-                       (if (consp tree)
-                           (cons (car tree)
-                                 (progn (setf previous (ends (car tree)))
-                                        (add-whitespace (cdr tree))))
-                           (progn (setf previous (ends tree)) nil)))))))
-      (let ((*string* (file-to-string file)))
-        (append
-         (add-whitespace
-          (append
-           (thread (prog1
-                       (with-open-file (input file)
-                         (mapcar #'cst-to-cons
-                                 (loop :with eof = '#:eof
-                                    :for n :from 0
-                                    :for form = (if (and count (>= n count))
-                                                    eof
-                                                    (cst-read input nil eof))
-                                    :until (eq form eof) :collect form)))
-                     (setf *comments* (nreverse *comments*))))
-           *comments*))
-         (when (< previous (length *string*))
-           (list (make-instance 'whitespace
-                   :raw (subseq *string* previous)
-                   :source (cons previous (length *string*))))))))))
+  (let ((*string* (file-to-string file))
+        (eclector.reader:*client* (make-instance 'second-climacs)))
+    (labels
+        ((make-space (start end)
+           (when (< start end)
+             (list (make-instance 'skipped-input-result
+                     :start start :end end :reason 'whitespace))))
+         (w/space (tree from to)
+           (etypecase tree
+             (list
+              (append
+               (iter (for subtree in tree)
+                     (appending (make-space from (start subtree)))
+                     (appending (w/space subtree (start subtree) (end subtree)))
+                     (setf from (end subtree)))
+               (make-space from to)))
+             (result
+              (when (subtypep (type-of tree) 'expression-result)
+                (setf (children tree)
+                      (w/space
+                       (children tree) (start tree) (end tree))))
+              (append
+               (make-space from (start tree))
+               (list tree)
+               (make-space (end tree) to))))))
+      (w/space (with-open-file (input file)
+                 (loop :with eof = '#:eof
+                    :for n :from 0
+                    :for form = (if (and count (>= n count))
+                                    eof
+                                    (read input nil eof))
+                    :until (eq form eof) :collect form))
+               0 (length *string*)))))
+
+(defun walk-forms+ (function forms)
+  (mapcar (lambda (form)
+            (etypecase form
+              (skipped-input-result (funcall function form))
+              (expression-result (walk-forms+ function (children form)))))
+          forms))
+
+(defun write-file-forms+ (forms)
+  "Re-prints the exact output as the original file."
+  (with-open-file (out "/tmp/out.lisp" :direction :output :if-exists :supersede)
+    (walk-forms+ [{format out "~a"} #'ast-text] lisp)))
 
 
 ;;; Interface to ast-diff for lisp source trees.
-(defmethod ast-equal-p ((ast-a atom-cst) (ast-b atom-cst))
-  (ast-equal-p (raw ast-a) (raw ast-b)))
+(defmethod source ((result result))
+  (subseq (string-pointer result) (start result) (end result)))
 
-(defmethod ast-equal-p ((ast-a comment) (ast-b comment))
-  (ast-equal-p (raw ast-a) (raw ast-b)))
+(defmethod ast-equal-p ((ast-a result) (ast-b result))
+  (ast-equal-p (source ast-a) (source ast-b)))
 
-(defmethod ast-equal-p ((ast-a whitespace) (ast-b whitespace))
-  (ast-equal-p (raw ast-a) (raw ast-b)))
+(defmethod ast-cost ((result expression-result))
+  ;; NOTE: Interesting question here about if we want comments to add
+  ;;       to the weight of a source expression.
+  (ast-cost (expression result)))
 
-(defmethod ast-text ((ast atom-cst))
-  (format nil "~a" (raw ast)))
+(defmethod ast-cost ((result skipped-input-result)) 1)
 
-(defmethod ast-text ((ast comment))
-  (raw ast))
+(defmethod ast-can-recurse ((ast-a expression-result) (ast-b expression-result))
+  (and (children ast-a) (children ast-b)))
 
-(defmethod ast-text ((ast whitespace))
-  (raw ast))
+(defmethod ast-can-recurse
+    ((ast-a skipped-input-result) (ast-b expression-result))
+  nil)
+
+(defmethod ast-can-recurse
+    ((ast-a expression-result) (ast-b skipped-input-result))
+  nil)
+
+(defmethod ast-can-recurse
+    ((ast-a skipped-input-result) (ast-b skipped-input-result))
+  nil)
+
+(defmethod ast-on-recurse ((ast expression-result))
+  (children ast))
+
+(defmethod ast-un-recurse ((ast expression-result) children)
+  (make-instance 'expression-result
+    :start (start (car children))
+    :end (end (lastcar children))
+    :expression (mapcar #'expression children)
+    :children children
+    :string-pointer (string-pointer (car children))))
+
+(defmethod ast-text ((result result))
+  (source result))
 
 
 ;;; Command-line interface to lisp differencing.
@@ -181,7 +207,7 @@
   (setf *note-level* level))
 
 (defun run-lisp-diff (&aux (self (argv0)) (args *command-line-arguments*)
-                        raw flags)
+                        raw flags (colorp t))
   "Run a lisp difference on *COMMAND-LINE-ARGUMENTS*."
   (flet ((report (fmt &rest args)
            (apply #'format *error-output* (concatenate 'string "~a: " fmt)
@@ -197,6 +223,7 @@ Compare Lisp source FILES AST by AST.
 
 Options:
  -r, --raw                 output diff in raw Sexp (default is as text)
+ -C, --no-color            inhibit color printing
  -v, --verbose [NUM]       verbosity level 0-4
 
 Built with ~a version ~a.~%"
@@ -205,6 +232,7 @@ Built with ~a version ~a.~%"
     ;; Argument handling and checking.
     (getopts (args :unknown :return)
       ("-r" "--raw" (setf raw t))
+      ("-C" "--no-color" (setf colorp nil))
       ("-v" "--verbose" (handle-set-verbose-argument
                          (parse-integer (pop args)))))
     (when (= (length args) 1)
@@ -228,11 +256,17 @@ Built with ~a version ~a.~%"
     (setf flags (list "-I" (pwd)))
     ;; Create the diff.
     (let ((diff
-           (ast-diff (read-file-forms-and-extras (car args))
-                     (read-file-forms-and-extras (second args)))))
+           (ast-diff (read-file-forms+ (car args))
+                     (read-file-forms+ (second args)))))
       ;; Print according to the RAW option.
       (if raw
           (writeln (ast-diff-elide-same diff) :readably t)
-          (print-diff diff))
+          (if colorp
+              (print-diff diff *standard-output*
+                          (format nil "~a[-" +color-RED+)
+                          (format nil "-]~a" +color-RST+)
+                          (format nil "~a{+" +color-GRN+)
+                          (format nil "+}~a" +color-RST+))
+              (print-diff diff)))
       ;; Only exit with 0 if the two inputs match.
       (quit (if (every [{eql :same} #'car] diff) 0 1)))))
