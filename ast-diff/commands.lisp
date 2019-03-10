b1aaa47b9e941cb4dd9dd6e7da8e55fb889f4f97
Eric Schulte <eschulte@grammatech.com>
2019-03-09 20:59:07 -0500

    Merge less verbose, works w/o projects or out-dir
    - It is now possible to merge single clang files.  The compilation
      database is parsed and applied to the files individually (using some
      logic from clang-project).
    
    - Output is now written to STDOUT when no out-dir is specified and
      when a single file is being merged.  This is more like standard diff
      or diff3 behavior.  An out-dir is still forced for directories.
    
    - Removed nearly all of the notes from the ast-merge command

--- ast-diff/commands.lisp
+++ ast-diff/commands.lisp
@@ -36,6 +36,9 @@
         :named-readtables
         :curry-compose-reader-macros
         :iterate
+        ;; TODO: Drop next two when importing compilation-db functions.
+        :split-sequence
+        :cl-ppcre
         :software-evolution-library
         :software-evolution-library/utility
         :software-evolution-library/command-line
@@ -50,22 +53,14 @@
         :software-evolution-library/software/javascript
         :software-evolution-library/software/javascript-project
         :software-evolution-library/software/lisp)
-  (:import-from :uiop :writeln :truenamize)
+  (:import-from :uiop :writeln :truenamize :absolute-pathname-p :nest)
   (:shadow :merge :ast-diff)
-  (:export :save-to))
+  (:export :ast-diff :ast-merge))
 (in-package :software-evolution-library/ast-diff/commands)
 (in-readtable :curry-compose-reader-macros)
 ;;; TODO: Work on clang-diff tool's git configuration (or maybe this
 ;;;       has to be implemented in clang-diff itself) to limit
 ;;;       application by extension.
-
-;;; TODO: I don't see why `save-to' is needed instead of just using `to-file'.
-(defgeneric save-to (soft out-dir sub))
-
-(defmethod save-to ((soft t) out-dir sub)
-  (let ((dest (make-pathname :directory (append out-dir (list sub)))))
-    (unless (probe-file dest)
-      (to-file (copy soft) dest))))
 
 (defun guess-language-from (&rest sources)
   (flet ((%guess-language-from (source)
@@ -98,6 +93,54 @@
               (("no-color" #\C) :type boolean :optional t
                :documentation "inhibit color printing")))))
 
+;;; Next two taken from clang-project.lisp.  Should be exported there
+;;; once we are able to remove the `project-path' nonsense.
+(defun compilation-db-entry-compiler (entry)
+  "Return the compiler in the compilation database ENTRY."
+  (or (first (split-sequence #\Space (aget :command entry)))
+      (first (aget :arguments entry))))
+
+(defun compilation-db-entry-flags (entry)
+  "Return the compiler flags in the compilation database ENTRY."
+  (flet ((flag-list-helper (entry)
+           "Get a massaged list of compiler flags from the ENTRY."
+           (nest
+            (mappend (lambda (arg) ; Split leading "-L".
+                       (split-sequence #\Space
+                         (replace-all arg "-L" "-L ")
+                         :remove-empty-subseqs t)))
+            (mappend (lambda (arg) ; Split leading "-I".
+                       (split-sequence #\Space
+                         (replace-all arg "-I" "-I ")
+                         :remove-empty-subseqs t)))
+            (or (mapcar (lambda (arg) ; Wrap quotes for the shell.
+                          (regex-replace
+                           "\"([^\"]*)\"" arg "'\"\\1\"'"))
+                        ;; Drop the first element of
+                        ;; arguments which is the compiler.
+                        (cdr (aget :arguments entry)))
+                (cdr (split-sequence
+                         #\Space (or (aget :command entry) "")
+                         :remove-empty-subseqs t))))))
+    (nest
+     ;; Remove the file being built from the flags.
+     (remove-if {equalp (aget :file entry)})
+     (iter (for f in (flag-list-helper entry))
+           (for p previous f)
+           (collect (if (or (string= p "-I") (string= p "-L"))
+                        ;; Ensure include/library paths
+                        ;; point to the correct location
+                        ;; and not a temporary build directory.
+                        (if (absolute-pathname-p f)
+                            f
+                            (merge-pathnames-as-directory
+                             (make-pathname :directory
+                                            (aget :directory entry))
+                             (make-pathname :directory
+                                            (list :relative f))))
+                        ;; Pass the flag thru.
+                        f))))))
+
 (defun create-software-from
     (path language
      &optional flags compiler compilation-database build-command)
@@ -111,7 +154,17 @@
                (when (and compiler (member language '(clang #| clang-project |#)))
                  (list :compiler compiler))
                (when compilation-database
-                 (list :compilation-database compilation-database))
+                 (case language
+                   (clang-project
+                    (list :compilation-database compilation-database))
+                   (clang
+                    (list :compiler (compilation-db-entry-compiler
+                                     (first compilation-database))
+                          :flags (compilation-db-entry-flags
+                                  (first compilation-database))))
+                   (t
+                    (error "language ~s doesn't support a compilation database"
+                           language))))
                (when (and compiler (subtypep language 'project))
                  (list :build-command build-command))))
              path))
@@ -149,7 +202,7 @@
                   diff)))
 
 (define-command ast-merge (my-file old-file your-file
-                               &spec +command-line-options+)
+                                   &spec +command-line-options+)
   "Merge changes from old-file->my-file and old-file->your-file."
   #.(format nil
             "~%Built from SEL ~a, and ~a ~a.~%"
@@ -160,37 +213,37 @@
   (setf *note-out* (list *error-output*))
   (unless (every #'resolve-file (list old-file my-file your-file))
     (exit-command ast-merge 2 (error "Missing source.")))
-  (setf out-dir (or out-dir (resolve-out-dir-from-source old-file))
-        old-file (truenamize old-file)
+  (setf old-file (truenamize old-file)
 	my-file (truenamize my-file)
 	your-file (truenamize your-file)
         language (or language (guess-language-from old-file my-file your-file)))
+  ;; Force OUT-DIR when running as a command line utility and merging
+  ;; whole directories.  We can't write multiple files to STDOUT.
+  (when (and (not uiop/image:*lisp-interaction*)
+             (not out-dir)
+             (directory-p old-file))
+    (setf out-dir (resolve-out-dir-from-source old-file))
+    (note 0 "Merging directories, out-dir set to ~a." out-dir))
+  ;; Don't write notes when we're writing merge results to STDOUT.
+  (unless out-dir (setf *note-level* 0))
 
-  (note 3 "Parameters:~%~S~%"
-        `((old-file . ,old-file)
-          (my-file . ,my-file)
-          (your-file . ,your-file)
-          (out-dir . ,out-dir)
-          (*note-level* . ,*note-level*)))
-
-  (note 3 "Creating software objects")
-
-  ;; TODO: clang-merge had a step "Create syled versions of the input files."
-  ;;       which has been removed here.  Not sure why it was necessary.
-
-  (note 3 "Performing merge")
   (multiple-value-bind (new-merged unstable)
       (apply #'converge
 	     (mapcar
-	      { create-software-from
-	      _ language flags compiler compilation-database build-command}
+	      {create-software-from
+	       _ language flags compiler compilation-database build-command}
 	      (list old-file my-file your-file)))
-    (if (directory-p old-file)
-	(save-to new-merged out-dir "merge")
-	(to-file new-merged (make-pathname :directory out-dir
-					   :name "merge")))
-    (if (not unstable)
-	(note 1 "No merge conflicts")
-	(note 0 "Merge conflicts:~%~a~%" unstable))
+    ;; Write the result, either to out-dir or to STDOUT.
+    (if out-dir
+        (if (directory-p old-file)
+            (to-file new-merged
+                     (make-pathname :directory (append out-dir "merged")))
+	    (to-file new-merged
+                     (resolve-store-path-from-out-dir-and-name
+                      out-dir
+                      (pathname-name old-file) "merged"
+                      (pathname-type old-file))))
+        (genome-string new-merged *standard-output*))
+
     (wait-on-manual manual)
     (exit-command ast-merge (if unstable 1 0) new-merged)))
