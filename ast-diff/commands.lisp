4d1304c42600a41ed92afe996c6b829ac4601c5f
Eric Schulte <eschulte@grammatech.com>
2019-03-19 21:19:45 -0400

    File-specific options for ast-merge
    The commit adds my-file, old-file, and your-file specific alternatives
    of +clang-command-line-options+, +project-command-line-options+, and
    +clang-project-command-line-options+ to the ast-merge command.

--- ast-diff/commands.lisp
+++ ast-diff/commands.lisp
@@ -112,8 +112,35 @@
                   (if (every [{eql :same} #'car] diff) 0 1)
                   diff)))
 
+(nest
+ (eval-when (:compile-toplevel :load-toplevel :execute))
+ (defparameter +ast-merge-command-line-options+)
+ (append +command-line-options+)
+ ;; Triples (base, left, right) of relevant options.
+ (mappend
+  (lambda (arg-spec)
+    (destructuring-bind
+          ((long short) &key type optional initial-value action documentation)
+        arg-spec
+      (declare (ignorable short))
+      (mapcar (lambda (which)
+                (append
+                 (list (list (concatenate 'string which "-" long)))
+                 (when type (list :type type))
+                 (when optional (list :optional optional))
+                 (when action (list :action action))
+                 (when initial-value (list :initial-value initial-value))
+                 (nest
+                  (when documentation)
+                  (list :documentation)
+                  (concatenate 'string documentation " for " which " file"))))
+              '("my" "old" "your")))))
+ (append +clang-command-line-options+
+         +project-command-line-options+
+         +clang-project-command-line-options+))
+
 (define-command ast-merge (my-file old-file your-file
-                                   &spec +command-line-options+)
+                                   &spec +ast-merge-command-line-options+)
   "Merge changes from old-file->my-file and old-file->your-file."
   #.(format nil
             "~%Built from SEL ~a, and ~a ~a.~%"
@@ -128,38 +155,46 @@
 	my-file (truenamize my-file)
 	your-file (truenamize your-file)
         language (or language (guess-language old-file my-file your-file)))
-  ;; Force OUT-DIR when running as a command line utility and merging
-  ;; whole directories.  We can't write multiple files to STDOUT.
-  (when (and (not uiop/image:*lisp-interaction*)
-             (not out-dir)
-             (directory-p old-file))
-    (setf out-dir (resolve-out-dir-from-source old-file))
-    (note 0 "Merging directories, out-dir set to ~a." out-dir))
-  ;; Don't write notes when we're writing merge results to STDOUT.
-  (unless out-dir (setf *note-level* 0))
+  (macrolet
+      ((expand-options-for-which-files (language which)
+         "Expand the options for WHICH calling `create-software' appropriately."
+         `(create-software
+           ,@(let ((pairs (mapcar
+                           «list [#'intern {concatenate 'string which "-"}
+                                           #'symbol-name]
+                                 #'identity»
+                           '(file compiler flags build-command
+                             artifacts compilation-database))))
+               (list* (caar pairs)
+                      :language language
+                      (mappend «list [#'make-keyword #'second] {cons 'or}»
+                               (cdr pairs)))))))
+    ;; Force OUT-DIR when running as a command line utility and merging
+    ;; whole directories.  We can't write multiple files to STDOUT.
+    (when (and (not uiop/image:*lisp-interaction*)
+               (not out-dir)
+               (directory-p old-file))
+      (setf out-dir (resolve-out-dir-from-source old-file))
+      (note 0 "Merging directories, out-dir set to ~a." out-dir))
+    ;; Don't write notes when we're writing merge results to STDOUT.
+    (unless out-dir (setf *note-level* 0))
 
-  (multiple-value-bind (new-merged unstable)
-      (apply #'converge
-	     (mapcar
-	      {create-software _
-                               :language language
-                               :compiler compiler
-                               :flags flags
-                               :build-command build-command
-                               :artifacts artifacts
-                               :compilation-database compilation-database}
-	      (list old-file my-file your-file)))
-    ;; Write the result, either to out-dir or to STDOUT.
-    (if out-dir
-        (if (directory-p old-file)
-            (to-file new-merged
-                     (make-pathname :directory (append out-dir (list "merged"))))
-	    (to-file new-merged
-                     (resolve-store-path-from-out-dir-and-name
-                      out-dir
-                      (pathname-name old-file) "merged"
-                      (pathname-type old-file))))
-        (genome-string new-merged *standard-output*))
+    (multiple-value-bind (new-merged unstable)
+        (converge
+         (expand-options-for-which-files language "OLD")
+         (expand-options-for-which-files language "MY")
+         (expand-options-for-which-files language "YOUR"))
+      ;; Write the result, either to out-dir or to STDOUT.
+      (if out-dir
+          (if (directory-p old-file)
+              (to-file new-merged
+                       (make-pathname :directory (append out-dir (list "merged"))))
+	      (to-file new-merged
+                       (resolve-store-path-from-out-dir-and-name
+                        out-dir
+                        (pathname-name old-file) "merged"
+                        (pathname-type old-file))))
+          (genome-string new-merged *standard-output*))
 
-    (wait-on-manual manual)
-    (exit-command ast-merge (if unstable 1 0) new-merged)))
+      (wait-on-manual manual)
+      (exit-command ast-merge (if unstable 1 0) new-merged))))
