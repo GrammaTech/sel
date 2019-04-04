6a8f358cf8366f0053e00ff1a05ec39b0a0a4930
Jason Ruchti <jruchti@grammatech.com>
2019-04-04 15:53:41 -0600

    Handle -L flags similarly to -I flags

--- software/clang-project.lisp
+++ software/clang-project.lisp
@@ -86,44 +86,47 @@ information on the format of compilation databases."))
   ;; Get flags from :arguments or :command field.  These fields handle quote
   ;; escaping differently; see
   ;; https://clang.llvm.org/docs/JSONCompilationDatabase.html.
-  (->> (iter (for f in (->> (or (mapcar (lambda (arg) ; Wrap quotes for the shell.
-                                          (regex-replace
-                                           "\"([^\"]*)\"" arg "'\"\\1\"'"))
-                                        ;; Drop the first element of
-                                        ;; arguments which is the compiler.
-                                        (cdr (aget :arguments entry)))
-                                (cdr (split-sequence
-                                         #\Space (or (aget :command entry) "")
-                                         :remove-empty-subseqs t)))
-                         (mappend (lambda (arg) ; Split leading "-I".
-                                    (split-sequence #\Space
-                                      (replace-all arg "-I" "-I ")
-                                      :remove-empty-subseqs t)))))
-             (for p previous f)
-             (collect
-                 (if (string= p "-I")
-                     (if (starts-with-subseq "/" f)
-                         (->> (ensure-directory-pathname f)
-                           (project-path clang-project))
-                         (->> (merge-pathnames-as-directory
-                               (make-pathname :directory
-                                              (aget :directory
-                                                    entry))
-                               (make-pathname :directory
-                                              (list :relative f)))
-                           (ensure-directory-pathname)
-                           (project-path clang-project)))
-                     f)))
-    (remove-if {string= (aget :file entry)})
-    (append (list "-I"
-                  (namestring (project-dir clang-project))))
-    (append (list "-I"
-                  (->> (merge-pathnames-as-directory
-                        (ensure-directory-pathname
-                         (aget :directory entry))
-                        (aget :file entry))
-                    (ensure-directory-pathname)
-                    (project-path clang-project))))))
+  (labels ((get-flags (entry)
+             (->> (or (mapcar (lambda (arg) ; Wrap quotes for the shell.
+                               (regex-replace
+                                "\"([^\"]*)\"" arg "'\"\\1\"'"))
+                              ;; Drop the first element of
+                              ;; arguments which is the compiler.
+                              (cdr (aget :arguments entry)))
+                      (cdr (split-sequence #\Space
+                             (or (aget :command entry) "")
+                             :remove-empty-subseqs t)))
+                  (mappend (lambda (arg) ; Split leading "-I" and "-L".
+                             (split-sequence #\Space
+                               (replace-all (replace-all arg "-I" "-I ")
+                                            "-L" "-L ")
+                               :remove-empty-subseqs t))))))
+    (->> (iter (for f in (get-flags entry))
+               (for p previous f)
+               (collect
+                   (if (or (string= p "-L") (string= p "-I"))
+                       (if (starts-with-subseq "/" f)
+                           (->> (ensure-directory-pathname f)
+                                (project-path clang-project))
+                           (->> (merge-pathnames-as-directory
+                                 (make-pathname :directory
+                                                (aget :directory
+                                                      entry))
+                                 (make-pathname :directory
+                                                (list :relative f)))
+                                (ensure-directory-pathname)
+                                (project-path clang-project)))
+                       f)))
+         (remove-if {string= (aget :file entry)})
+         (append (list "-I"
+                       (namestring (project-dir clang-project))))
+         (append (list "-I"
+                       (->> (merge-pathnames-as-directory
+                             (ensure-directory-pathname
+                              (aget :directory entry))
+                             (aget :file entry))
+                            (ensure-directory-pathname)
+                            (project-path clang-project)))))))
 
 ;;; TODO: The next two functions should be used instead of
 ;;; `get-compiler' and `get-flags' respectively in
