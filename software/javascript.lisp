3933785a8876dc3a103ed4d387136021e5b57fa0
Paul Dietz <pdietz@grammatech.com>
2019-06-10 11:08:21 -0400

    Add ast-kind method for javascript

--- software/javascript.lisp
+++ software/javascript.lisp
@@ -68,6 +68,8 @@
 ;;; Javascript ast data structures
 (define-ast (javascript-ast (:conc-name ast)))
 
+(defmethod ast-kind ((ast javascript-ast)) :javascript)
+
 (defmethod print-object ((obj javascript-ast-node) stream)
   "Print a representation of the javascript-ast-node OBJ to STREAM.
 * OBJ clang-ast to print
