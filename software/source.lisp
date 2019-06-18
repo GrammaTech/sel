4b059dc57e3eebe4d1183a6106875cd0714e5214
Paul Dietz <pdietz@grammatech.com>
2019-06-18 09:21:38 -0400

    Add a genome-lines-mixin
    This mixin adds a slot that is the source file broken
    down into lines.  The slot is automatically invalidated
    when genome is updated, and (on demand) is obtained
    from the genome by decomposing it into a vector of
    strings (not including the line terminator).   There
    is also a slot for indicating if the last line was
    terminated by the line terminator (default Newline)
    or by the end of the genome.

--- software/source.lisp
+++ software/source.lisp
@@ -33,6 +33,14 @@
              :copier :none))
   (:documentation "Raw source code software representation."))
 
+(defclass genome-lines-mixin ()
+  ((genome-lines :type vector :accessor genome-lines
+                 :documentation "Mapping from line numbers (starting at zero) to positions
+in genome.")
+   (last-line-terminated? :type boolean :accessor last-line-terminated?
+                          :documentation "If true, last line was terminated by the separator"))
+  (:documentation "Mixin for recording lines of a source"))
+
 (defmethod phenome ((obj source) &key (bin (temp-file-name)))
   "Compile OBJ to create an executable version of the software
 on the filesystem at BIN."
@@ -56,6 +64,41 @@ on the filesystem at BIN."
   "Return the source code of OBJ, optionally writing to STREAM"
   (let ((genome (or (genome obj) "")))
     (if stream (write-string genome stream) genome)))
+
+(defmethod (setf genome) :after (v (obj genome-lines-mixin))
+  (slot-makunbound obj 'genome-lines))
+
+(defmethod slot-unbound (class (obj genome-lines-mixin)
+                         (slot-name (eql 'genome-lines)))
+  ;; Compute GENOME-LINES when needed
+  (let* ((s (genome-string obj))
+         (ter (make-string 1 :initial-element #\Newline
+                           :element-type 'base-char))
+         (len (length s))
+         (ter-len (length sep))
+         (etype (array-element-type s))
+         (pos 0))
+    ;; Lines will be displaced into genome-string, so we don't
+    ;; waste space holding two copies of the program.
+    ;; Lines do NOT include line terminator character (ter)
+    (setf (last-line-terminated? obj) t)
+    (let ((lines (iter (while (<= pos len))
+                       (let* ((next (search ter s :start pos))
+                              (line-len
+                               (if next
+                                   (prog1 (- next pos)
+                                     (setf pos (+ next ter-len)))
+                                   ;; Last line was nonempty and did not end in
+                                   ;; the expected terminator character(s)
+                                   (prog1 (- len pos)
+                                     (setf (last-line-terminated? obj) nil
+                                           pos len)))))
+                         (collecting (make-array (list line-len)
+                                                 :element-type etype
+                                                 :displaced-to s
+                                                 :displaced-index-offset pos))))))
+      (setf (genome-lines obj)
+            (make-array (list (length lines)) :intial-contents lines)))))
 
 (defmethod from-file ((obj source) path)
   "Initialize OBJ with the contents of PATH."
