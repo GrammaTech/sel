2d4afae986e6e5b2cc5e9417e4fd9db920adbab8
Eric Schulte <eschulte@grammatech.com>
2019-07-12 23:26:03 -0400

    Docstring indentation

--- command-line-rest.lisp
+++ command-line-rest.lisp
@@ -25,26 +25,26 @@
     (name args pre-help post-help &rest body)
   "Define a function, executable, and a REST server function and executable.
 
-   Invokes `define-command' on NAME ARGS PRE-HELP POST-HELP and BODY to
-   define the NAME function and the RUN-NAME command-line executable
-   entry point.  See the definition of `define-command' for more
-   information on these arguments.
+Invokes `define-command' on NAME ARGS PRE-HELP POST-HELP and BODY to
+define the NAME function and the RUN-NAME command-line executable
+entry point.  See the definition of `define-command' for more
+information on these arguments.
 
-   Use the above arguments and the additional ENVIRONMENT and STATUS
-   keywords to define an asynchronous REST entry point which runs the
-   function NAME asynchronously returning a job ID and another entry
-   point which may be used to retrieve the status of the async job.  A
-   new RUN-NAME-REST-SERVER command-line executable entry point is
-   defined.
+Use the above arguments and the additional ENVIRONMENT and STATUS
+keywords to define an asynchronous REST entry point which runs the
+function NAME asynchronously returning a job ID and another entry
+point which may be used to retrieve the status of the async job.  A
+new RUN-NAME-REST-SERVER command-line executable entry point is
+defined.
 
-   ENVIRONMENT
-   :   List of variables which should be let-bound around the execution
-   of the REST end-point.
+ENVIRONMENT
+:   List of variables which should be let-bound around the execution
+    of the REST end-point.
 
-   STATUS
-   :   A function which will be invoked in the dynamic environment of the
-   running job to return the status of the job or, when finished, to
-   return the result."
+STATUS
+:   A function which will be invoked in the dynamic environment of the
+    running job to return the status of the job or, when finished, to
+    return the result."
   ;; Split the args to pull out types and pass names to `define-command'.
   ;; There isn't much we can do here about rest args, due to types. We leave
   ;; the binding unused for now.
