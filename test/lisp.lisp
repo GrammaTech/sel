194f67e5aaefc454084921f08a9b2fb049fc33d0
Paul M. Rodriguez <prodriguez@grammatech.com>
2020-03-18 15:43:47 -0500

    Add (failing) tests
    For guidance on how the AST should work.

--- test/lisp.lisp
+++ test/lisp.lisp
@@ -17,6 +17,12 @@
   (:import-from :uiop :nest)
   (:import-from :asdf
                 :system-relative-pathname)
+  (:import-from :functional-trees
+                :traverse-nodes)
+  (:import-from :fset
+                :convert)
+  (:import-from :trivia
+                :match)
   (:shadowing-import-from
    :closer-mop
    :standard-method :standard-class :standard-generic-function
@@ -33,3 +39,34 @@
    (from-file (make-instance 'lisp))
    (namestring)
    (system-relative-pathname "software-evolution-library" "test/lisp.lisp")))
+
+(deftest read-eval-preserved ()
+  (let ((ast (convert 'lisp-ast "(defvar *day-seconds* (* 24 60 60))"))
+        (quit nil)
+        found?)
+    (traverse-nodes ast
+                    (lambda (node)
+                      (if (typep node 'expression-result)
+                          (if (equal (expression node) '(* 24 60 60))
+                              (progn
+                                (setf found? t)
+                                quit)
+                              :keep-going)
+                          :keep-going)))
+    (is found?)))
+
+(deftest read-conditional-preserved ()
+  (let* ((ast (convert 'lisp-ast "(list #+sbcl :sbcl #-sbcl :not-sbcl)"))
+         (quit nil)
+         found-sbcl? found-not-sbcl?)
+    (traverse-nodes ast
+                    (lambda (node)
+                      (if (typep node 'expression-result)
+                          (case (expression node)
+                            (:sbcl (setf found-sbcl? t)
+                                   quit)
+                            (:not-sbcl (setf found-not-sbcl? t)
+                                       quit)
+                            (t :keep-going))
+                          :keep-going)))
+    (is (and found-sbcl? found-not-sbcl?))))
