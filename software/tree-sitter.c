206035003a3d3e4688b818102ca2a692b067589e
Eric Schulte <eschulte@grammatech.com>
2021-02-20 17:04:11 -0500

    Python CFFI tree-sitter implementation begins

--- software/tree-sitter.c
+++ software/tree-sitter.c
@@ -15,5 +15,24 @@ void stop(){
 }
 
 cl_object convert(char *source){
-  return NULL;
+  cl_object text = ecl_cstring_to_base_string_or_nil(source);
+  return cl_funcall(3, c_string_to_object("convert"), c_string_to_object(":python"), text);
+}
+
+cl_object eval(char *source){
+  cl_env_ptr env = ecl_process_env();
+  ECL_CATCH_ALL_BEGIN(env) {
+    /*
+     * Code that is protected. Uncaught lisp conditions, THROW,
+     * signals such as SIGSEGV and SIGBUS may cause jump to
+     * this region.
+     */
+    return cl_eval(c_string_to_object(source));
+  } ECL_CATCH_ALL_IF_CAUGHT {
+    /*
+     * If the exception, lisp condition or other control transfer
+     * is caught, this code is executed.
+     */
+    return ecl_make_keyword("ERROR");
+  } ECL_CATCH_ALL_END;
 }
