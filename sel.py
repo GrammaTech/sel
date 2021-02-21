3b76a93a7b3f4ceb74cff65dd68b65683c2615c4
Eric Schulte <eschulte@grammatech.com>
2021-02-21 16:55:30 -0500

    Python library starting to turn over

--- sel.py
+++ sel.py
@@ -22,12 +22,14 @@ cpp = lib.CPP
 
 def ecl_mapcar(fn, ecl_list):
     results = []
-    while element := lib.car(ecl_list):
+    while True:
+        element = lib.car(ecl_list)
         if lib.null(element):
-            break;
+            break
         results.append(fn(element))
         ecl_list = lib.cdr(ecl_list)
-    return result.reverse()
+    results.reverse()
+    return results
 
 def to_string(cdata) -> str:
     return ffi.string(cdata, lib.get_last_string_length())
@@ -36,21 +38,25 @@ class AST:
     def __init__(self, language:int = unknown_language, source:str="", handle=None) -> None:
         if handle == None:
             self.handle = lib.convert(language, source.encode('ascii'))
+        else:
+            self.handle = handle
 
     # NOTE: Do we need to explicitly free this on the ECL side?
     # def __del__(self) -> None:
 
     def ast_at_point(self, line:int, column:int):
-        return AST.new(lib.ast_at_point(self, line, column));
+        return AST(lib.ast_at_point(self, line, column));
 
     def type(self):
         return to_string(lib.type(self.handle))
 
     def children(self):
-        ecl_mapcar(lambda child: AST.new(handle=child), lib.children(self.handle))
+        return ecl_mapcar(lambda child: AST(handle=child), lib.children(self.handle))
 
     def child_slots(self) -> [str]:
-        ecl_mapcar(lambda slot: string(slot), lib.child_slots(self.handle))
+        return ecl_mapcar(lambda slot: (to_string(lib.to_string(lib.car(slot))),
+                                        lib.to_short(lib.cdr(slot))),
+                          lib.child_slots(self.handle))
 
     def source_text(self) -> str:
         return to_string(lib.source_text(self.handle))
