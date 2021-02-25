b17b85b56653de6f5c57bfaf18a179fffe19e32a
Eric Schulte <eschulte@grammatech.com>
2021-02-25 08:29:51 -0500

    More Python Library test infrastructure

--- python/test.py
+++ python/test.py
@@ -1,27 +1,50 @@
-# AST creation
-# AST source text
-# AST at point
-# AST children
-# AST children-slots
-# AST prefix
-# AST suffix
-# AST hash
-# AST type
-# AST class
-# AST language
+import os
+import unittest
+from sel import AST
 
-# AST accessors
-# function name
-# function parameters
-# function body
-# call-function
-# call-arguments
+class TestDriver(unittest.TestCase):
 
-# More like light weight static analysis
-# AST parent
-# AST enclosing function
-# AST in scope names
-# AST defined functions
-# AST callsites
-# AST callsite signatures
-# AST callsite module
+
+    def tearDown(self):
+        return
+
+
+    def setUp(self):
+        return
+
+
+    # AST creation
+    # AST source text
+    def test_ast_creation_from_source(self):
+        source = "x + 88"
+        self.assertTrue(source == AST(source=source).source_text())
+
+
+    # AST at point
+    # AST children
+    # AST children-slots
+    # AST prefix
+    # AST suffix
+    # AST hash
+    # AST type
+    # AST class
+    # AST language
+
+    # AST accessors
+    # function name
+    # function parameters
+    # function body
+    # call-function
+    # call-arguments
+
+    # More like light weight static analysis
+    # AST parent
+    # AST enclosing function
+    # AST in scope names
+    # AST defined functions
+    # AST callsites
+    # AST callsite signatures
+    # AST callsite module
+
+if __name__ == "__main__":
+    unittest.main()
