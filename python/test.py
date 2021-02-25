dec2c33bbf3e029429485f6934c72536d94d32f8
Eric Schulte <eschulte@grammatech.com>
2021-02-25 06:40:33 -0500

    Start to stub out tests

---
+++ python/test.py
@@ -0,0 +1,27 @@
+# AST creation
+# AST source text
+# AST at point
+# AST children
+# AST children-slots
+# AST prefix
+# AST suffix
+# AST hash
+# AST type
+# AST class
+# AST language
+
+# AST accessors
+# function name
+# function parameters
+# function body
+# call-function
+# call-arguments
+
+# More like light weight static analysis
+# AST parent
+# AST enclosing function
+# AST in scope names
+# AST defined functions
+# AST callsites
+# AST callsite signatures
+# AST callsite module
