c854497161e70d568fa40f80cade6bd6be43667b
Eric Schulte <eschulte@grammatech.com>
2021-03-10 20:53:21 -0500

    More debugging (unsuccessfully)

--- software/tree-sitter-test.c
+++ software/tree-sitter-test.c
@@ -2,24 +2,45 @@
 #include <locale.h>
 #include <stdio.h>
 #include <string.h>
+#include <wchar.h>
 #include "tree-sitter.h"
 
-#define check(NAME, LEFT, RIGHT) if(LEFT == RIGHT){ printf("PASS\n"); } else { \
-    printf("FAIL: %d != %d for " #NAME "\n", LEFT, RIGHT);              \
+#define check(NAME, TYPE, LEFT, RIGHT) if(LEFT == RIGHT){ printf("PASS\n"); } else { \
+    printf("FAIL: "#TYPE" != "#TYPE" for "#NAME"\n", LEFT, RIGHT);            \
     success = 1;                                                        \
   }
 
+/*
 int main(int argc, char** argv){
   int success = 0;
   start();
   char* source = "x + 88";
   cl_object ast = convert(PYTHON, source);
-  printf("Source length %zu\n", get_last_string_length());
   printf("AST pointer %p\n", (void*)ast);
   show(ast);
-  check(LANGUAGE, ast_language(ast), PYTHON);
-  check(TYPE, ast_type(ast), UNKNOWN_TYPE);
-  check(SOURCE-TEXT-LENGTH, strlen(source), strlen(source_text(ast)));
+
+  check(LANGUAGE, %d, ast_language(ast), PYTHON);
+  check(TYPE, %d, ast_type(ast), UNKNOWN_TYPE);
+  check(SOURCE-TEXT-LENGTH, %lu, strlen(source), wcslen(source_text(ast)));
+
   stop();
   return success;
+}
+*/
+
+int main(int argc, char** argv){
+  start();
+
+  char* source = "x + 88";
+  cl_object convert = c_string_to_object("convert");
+  cl_object symbol = ecl_make_symbol("PYTHON-AST",
+                                     "SOFTWARE-EVOLUTION-LIBRARY/SOFTWARE/TREE-SITTER");
+  cl_object str = ecl_cstring_to_base_string_or_nil(source);
+  cl_object ast = cl_funcall(3, convert, symbol, str);
+  show(convert);
+  show(symbol);
+  show(str);
+  show(ast);
+
+  return 0;
 }
