82a68b4befc08aa9482b6c30a8c625cf1932d15d
Jason Ruchti <jruchti@grammatech.com>
2021-03-25 10:22:51 -0600

    Remove unused/commented out fields in setup.py
    Some of these related to the previous CFFI-based
    approach.

--- python/setup.py
+++ python/setup.py
@@ -100,35 +100,6 @@ setup(
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
     python_requires='>=3.6',
 
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],  # Optional
-
-    # Handled by CFFI below.
-    #
-    # # If there are data files included in your packages that need to be
-    # # installed, specify them here.
-    # package_data={  # Optional
-    #     'tree-sitter': ['software/tree-sitter--all-systems.dylib'],
-    # },
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-    #     'console_scripts': [
-    #         'parse-tree=sel:parse-tree',
-    #     ],
-    # },
-
     # List additional URLs that are relevant to your project as a dict.
     #
     # This field corresponds to the "Project-URL" metadata fields:
