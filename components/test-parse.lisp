922340f442814869319892b0c12cd1dd3c2e102a
Paul F. Dietz <pdietz@grammatech.com>
2021-04-28 09:48:56 -0400

    Add --error-pattern argument to test parse
    This enables more focused reduction of specific errors.

--- components/test-parse.lisp
+++ components/test-parse.lisp
@@ -6,7 +6,8 @@
   (:use :gt/full
         :software-evolution-library
         :software-evolution-library/command-line
-   :software-evolution-library/software/parseable)
+        :cl-ppcre
+        :software-evolution-library/software/parseable)
   (:import-from :software-evolution-library/software/tree-sitter
                 :parsing-error :rule-matching-error)
   (:export test-parse run-test-parse))
@@ -23,7 +24,9 @@
                     :action #'handle-eval
                     :documentation "eval STRING as lisp code")
       (("error-type" #\E) :type string :action #'read-from-string
-                     :documentation "type of errors to be recognized as interesting")
+                          :documentation "type of errors to be recognized as interesting")
+      (("error-pattern" #\P) :type string :action #'identity
+                             :documentation "CL-PPCRE pattern for errors to be considered interesting")
       (("language" #\L) :type string :initial-value "c"
                         :documentation
        "language of input files (e.g. c, c++, lisp, or javascript)"))))
@@ -37,11 +40,14 @@ These codes are chosen to work with creduce."
               +software-evolution-library-version+
               (lisp-implementation-type) (lisp-implementation-version))
   (declare (ignorable load eval))
-  (handler-case
-      (progn (genome (create-software source :language language))
-             (exit-command test-parse 1 nil))
-    (parsing-error (e) (exit-command test-parse 1 e))
-    (error (e) (if (or (null error-type)
-                         (typep e error-type))
-                   (exit-command test-parse 0 e)
-                   (exit-command test-parse 1 e)))))
+  (flet ((interesting? (e)
+           (or (and error-type (typep e error-type))
+               (and error-pattern (cl-ppcre:scan error-pattern (format nil "~s" e)))
+               (and (not error-type) (not error-pattern)))))
+    (handler-case
+        (progn (genome (create-software source :language language))
+               (exit-command test-parse 1 nil))
+      (parsing-error (e) (exit-command test-parse 1 e))
+      (error (e) (if (interesting? e)
+                     (exit-command test-parse 0 e)
+                     (exit-command test-parse 1 e))))))
