1b135a6240937d634371c600d12c51f9e5a9132f
Paul M. Rodriguez <prodriguez@grammatech.com>
2021-06-04 10:46:35 -0500

    Don't consider semicolons inserted by the output rule as children
    That breaks anything that expects children to have an AST path

--- software/javascript.lisp
+++ software/javascript.lisp
@@ -14,6 +14,9 @@
 (create-tree-sitter-language "javascript")
 ;;;===================================================
 
+(defmethod children ((ast javascript-ast))
+  (remove-if (of-type 'javascript-\;) (call-next-method)))
+
 (defmethod transform-parse-tree
     ((language (eql ':javascript))
      (class (eql 'javascript-lexical-declaration))
