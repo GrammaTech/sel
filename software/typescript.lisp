b5dd38066513eef710d4de8baaa0d01bede5fe43
Paul M. Rodriguez <prodriguez@grammatech.com>
2021-10-04 09:11:45 -0500

    Allow parsing typescript as typescript (not typescript-typescript)

--- software/typescript.lisp
+++ software/typescript.lisp
@@ -14,3 +14,15 @@
 (create-tree-sitter-language "typescript/tsx")
 (create-tree-sitter-language "typescript/typescript")
 ;;;===================================================
+
+(defmethod from-file ((class (eql 'typescript)) file)
+  (from-file 'typescript-typescript file))
+
+(defmethod from-file ((software typescript) file)
+  (from-file (change-class software 'typescript-typescript) file))
+
+(defmethod from-string ((class (eql 'typescript)) string)
+  (from-string 'typescript-typescript string))
+
+(defmethod from-string ((software typescript) string)
+  (from-string (change-class software 'typescript-typescript) string))
