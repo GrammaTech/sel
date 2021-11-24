9b9432968a6b91751f47a01875a7e45f7c0a6e03
Paul M. Rodriguez <prodriguez@grammatech.com>
2021-11-24 09:38:20 -0600

    Add missing dependency for tree-sitter-dev

--- software/tree-sitter-dev.lisp
+++ software/tree-sitter-dev.lisp
@@ -1,6 +1,7 @@
 (defpackage :software-evolution-library/software/tree-sitter-dev
   (:use :software-evolution-library/software/tree-sitter)
   (:import-from :cmd)
+  (:import-from :stefil+)
   (:documentation "Load this package for tree-sitter dev and debug tools."))
 (in-package :software-evolution-library/software/tree-sitter)
 
