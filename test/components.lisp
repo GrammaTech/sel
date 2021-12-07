f76557bb05172684ff16d1e8fb67de9cf2241d83
Roger Corman <roger@corman.net>
2021-12-06 17:22:25 -0800

    Added test cases for each type of test-suites.

--- test/components.lisp
+++ test/components.lisp
@@ -6,7 +6,10 @@
    #+gt :testbot
    :software-evolution-library/test/util
    :stefil+
-   :software-evolution-library/components/test-suite)
+   :software-evolution-library/components/test-suite
+   :software-evolution-library/command-line)
+  (:import-from :software-evolution-library/components/test-suite
+                :full-tests)
   (:export :test-components))
 (in-package :software-evolution-library/test/components)
 (in-readtable :curry-compose-reader-macros)
@@ -26,3 +29,43 @@
 (deftest timeout-test ()
   (let ((*process-kill-timeout* 0.5))
     (is (= 143 (nth-value 2 (run-test "/tmp" (make-sleeper 3)))))))
+
+(defun test-suite-test-dir (name)
+  (make-pathname :directory (append +etc-dir+ (list name))))
+
+(deftest all-tests-suite-test ()
+  (if-let* ((suite (is (create-all-tests-suite
+                        (namestring
+                         (merge-pathnames "tests.sh"
+                                          (test-suite-test-dir "gcd")))
+                        :time-limit 60)))
+            (num-cases (length (test-cases suite))))
+    (is (= num-cases 1))))
+
+(defparameter *test-suite* nil)
+
+(deftest test-suite-test ()
+  (if-let* ((suite (is (create-test-suite
+                        (namestring
+                         (merge-pathnames "tests.sh"
+                                          (test-suite-test-dir "gcd")))
+                        10
+                        :time-limit 60)))
+            (num-cases (length (test-cases suite))))
+    (is (= num-cases 10))))
+
+(deftest random-test-suite-test ()
+  (if-let* ((suite (is (create-random-test-suite
+                        (create-test-suite
+                         (namestring
+                          (merge-pathnames "tests.sh"
+                                           (test-suite-test-dir "gcd")))
+                         20
+                         :time-limit 60)
+                        5)
+                       "Could not create random-test-suite"))
+            (full-test-count (length (test-cases (full-tests suite))))
+            (num-cases (length (test-cases suite))))
+    (progn
+      (is (= num-cases 5) "Wrong number of test cases in random-test-suite")
+      (is (= full-test-count 20) "Wrong number of cases in full set"))))
