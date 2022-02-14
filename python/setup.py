e35ee38c37d22665ac9b084a38810d6454c81ded
Jason Ruchti <jruchti@grammatech.com>
2022-02-14 13:32:18 -0700

    Bump python library patch version

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="asts",
-    version="0.5.0",
+    version="0.5.1",
     description="A library for programmatic software modification",
     long_description=long_description,
     long_description_content_type="text/markdown",
