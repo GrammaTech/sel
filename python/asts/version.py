6bf0dff23067fc0ef745a4addb69ca9e8cf0bdac
Jason Ruchti <jruchti@grammatech.com>
2022-09-01 17:10:27 -0600

    Moving package information into version.py

---
+++ python/asts/version.py
@@ -0,0 +1,15 @@
+__packagename__ = "asts"
+__description__ = "A library for programmatic software modification"
+__version__ = "0.6.8"
+__pythonbaseversion__ = "3.6.0"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+]
--- python/setup.py
+++ python/setup.py
@@ -2,12 +2,19 @@
 
 See: https://grammatech.github.io/sel
 """
+from importlib.util import module_from_spec, spec_from_file_location
 from setuptools import find_packages, setup
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+spec = spec_from_file_location(
+    "pkginfo.version",
+    path.join(this_directory, "asts/version.py"),
+)
+pkginfo = module_from_spec(spec)
+spec.loader.exec_module(pkginfo)
+
+setup_requirements = ["wheel"]
 
 with open(path.join(this_directory, "requirements.txt")) as fd:
     requirements = fd.readlines()
@@ -15,33 +22,26 @@ with open(path.join(this_directory, "requirements.txt")) as fd:
 with open(path.join(this_directory, "requirements-dev.txt")) as fd:
     development_requirements = fd.readlines()
 
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-    name="asts",
-    version="0.6.8",
-    description="A library for programmatic software modification",
+    name=pkginfo.__packagename__,
+    version=pkginfo.__version__,
+    description=pkginfo.__description__,
+    classifiers=pkginfo.__classifiers__,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/grammatech/sel",
     author="Eric Schulte and GrammaTech",
     license="GPLv3+",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
     keywords="software-engineering, source, program-synthesis",
     packages=find_packages(exclude=["test"]),
     package_data={"asts": ["lib*", "tree-sitter*"]},
-    setup_requires=["wheel"],
+    setup_requires=setup_requirements,
     install_requires=requirements,
     extras_require={"dev": development_requirements},
-    python_requires=">=3.6",
+    python_requires=f">={pkginfo.__pythonbaseversion__}",
     project_urls={
         "Bug Reports": "https://github.com/grammatech/sel/issues",
         "Source": "https://github.com/grammatech/sel/",
