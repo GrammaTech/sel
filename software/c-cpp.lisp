60b559776b27400352d2f5da72403075a7a591a4
Paul M. Rodriguez <prodriguez@grammatech.com>
2022-09-06 14:42:08 -0500

    Fix indentation

--- software/c-cpp.lisp
+++ software/c-cpp.lisp
@@ -32,14 +32,14 @@
   "Return a keyword giving the context for AST."
   (etypecase ast
     ((or c/cpp-identifier c/cpp-type-identifier cpp-qualified-identifier)
-   (cond ((get-declaration-id :tag ast)
-          :type)
-         ((get-declaration-id :type ast)
-          :type)
-         ((get-declaration-id :variable ast)
-          :variable)
-         ((get-declaration-id :function ast)
-          :function)))))
+     (cond ((get-declaration-id :tag ast)
+            :type)
+           ((get-declaration-id :type ast)
+            :type)
+           ((get-declaration-id :variable ast)
+            :variable)
+           ((get-declaration-id :function ast)
+            :function)))))
 
 (defun binary-expression->cast-expression (ast-type ast)
   "Converts BINARY-EXPRESSION into its corresponding cast expression."
