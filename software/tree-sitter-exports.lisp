42a297f4c1afa3165a6e2570b586021d84e0e1db
Paul M. Rodriguez <prodriguez@grammatech.com>
2022-09-06 14:43:16 -0500

    Remove unused file

--- software/tree-sitter-exports.lisp
+++
@@ -1,4 +0,0 @@
-(uiop:define-package :software-evolution-library/software/tree-sitter-exports
-    (:use :gt/full)
-  (:export :*tree-sitter-exports*))
-(in-package :software-evolution-library/software/tree-sitter-exports)
