78376fe02eee7c7af944e8356daada6893da7dca
Paul M. Rodriguez <prodriguez@grammatech.com>
2023-07-17 10:51:33 -0500

    Add functions to look up modules by name

--- software/cpp-project.lisp
+++ software/cpp-project.lisp
@@ -18,11 +18,13 @@
 (in-package :software-evolution-library/software/cpp-project)
 (in-readtable :curry-compose-reader-macros)
 
-(defparameter *cpp-extensions*
-  '("h" "hpp" "cpp" "cp" "cc" "cxx"
-    ;; Module unit extensions. Visual Studio uses .ixx, Clang
+(defparameter *cpp-module-extensions*
+  '(;; Module unit extensions. Visual Studio uses .ixx, Clang
     ;; uses the extensions ending with -m.
-    "ixx" "cppm" "ccm" "cxxm" "c++m")
+    "ixx" "cppm" "ccm" "cxxm" "c++m"))
+
+(defparameter *cpp-extensions*
+  `("h" "hpp" "cpp" "cp" "cc" "cxx" ,@*cpp-module-extensions*)
   "List of extensions we will consider for evolving.")
 
 (define-software cpp-project (c/cpp-project)
@@ -56,5 +58,53 @@
        (namespace node ""))
       (root (namespace root ""))
       (t (namespace attrs-root "")))))
+
+(defun relative-module-defaults (importing-path
+                                 importing-name
+                                 imported-name)
+  "Compute the base pathname (no extension) for an imported module in
+the same directory as IMPORTING-PATH.
+
+Note IMPORTING-NAME is only needed when IMPORTED-NAME is a module
+partition."
+  (declare (pathname importing-path)
+           ((or string null) importing-name)
+           (string imported-name))
+  (let* ((file-name
+          (if (string^= ":" imported-name)
+              (progn
+                (unless importing-name
+                  (error "Cannot import a module partition directly"))
+                (string+ importing-name "-" (drop-prefix ":" imported-name)))
+              imported-name)))
+    (make-pathname :name file-name
+                   :defaults importing-path
+                   :type nil)))
+
+(defun find-relative-module (defaults)
+  "Find an imported module based on DEFAULTS."
+  (some (lambda (type)
+          (file-exists-p
+           (make-pathname :defaults defaults
+                          :type type)))
+        *cpp-module-extensions*))
+
+(defun find-module (defaults list &key (key #'identity))
+  "Find a module based on DEFAULTS in LIST, a list of pathname designators."
+  (let ((target-name (pathname-name defaults)))
+    (block nil
+      (with-item-key-function (key)
+        (dolist (item list)
+          (let ((path (pathname (key item))))
+            (when (and (equal (pathname-name path)
+                              target-name)
+                       (member (pathname-type path)
+                               *cpp-module-extensions*
+                               :test #'equal))
+              (return item))))))))
+
+(defun find-project-module (project defaults)
+  "Find a module that satisfies DEFAULTS in PROJECT."
+  (cdr (find-module defaults (evolve-files project) :key #'car)))
 
 ) ; #+:TREE-SITTER-CPP
