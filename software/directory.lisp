bc70ea90266c261b5d7af2d2a4ec1a927ed2b9f1
Paul M. Rodriguez <prodriguez@grammatech.com>
2023-12-15 14:36:21 -0600

    Recompute subroot mappings after mutating a directory genome

--- software/directory.lisp
+++ software/directory.lisp
@@ -186,6 +186,11 @@
 (defmethod (setf genome) ((new ast) (obj directory-project))
   (setf (slot-value obj 'genome) new))
 
+(defmethod (setf genome) :after ((new ast) (obj directory-project))
+  (when-let (attrs (bound-value 'attrs:*attrs*))
+    (when (eql obj (attrs:attrs-root attrs))
+      (attrs:recompute-subroot-mapping))))
+
 (defmethod with ((project directory-project)
                  (old parseable)
                  &optional new)
