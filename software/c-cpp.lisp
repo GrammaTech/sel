c4219ea66eacc3a4dba3e32f3a8a9cf11c413a31
Paul M. Rodriguez <prodriguez@grammatech.com>
2024-12-28 14:48:35 -0600

    Add a function-body method for C/C++ function declarators

--- software/c-cpp.lisp
+++ software/c-cpp.lisp
@@ -278,6 +278,9 @@ Otherwise, use heuristics."
 (defmethod primitive-type-p ((ast c/cpp-type-descriptor))
   (primitive-type-p (c/cpp-type ast)))
 
+(defmethod function-body ((ast c/cpp-function-declarator))
+  nil)
+
 (defmethod function-name ((ast c/cpp-function-definition))
   (source-text (definition-name-ast ast)))
 
