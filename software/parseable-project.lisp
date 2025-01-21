1957fcc0e36c37d5d128d0889131837c07407782
Paul M. Rodriguez <prodriguez@grammatech.com>
2025-01-21 16:57:40 -0600

    Invalidate the project itself when updating

--- software/parseable-project.lisp
+++ software/parseable-project.lisp
@@ -10,13 +10,15 @@
         :software-evolution-library/software/parseable
         :software-evolution-library/software/project)
   (:import-from :functional-trees/attrs :attrs-root)
+  (:local-nicknames
+   (:attrs :functional-trees/attrs))
   (:export
     :parseable-project
     :project-parse-failures))
 (in-package :software-evolution-library/software/parseable-project)
 (in-readtable :curry-compose-reader-macros)
 
-(define-software parseable-project (project attrs-root)
+(define-software parseable-project (project attrs:attrs-root)
   ((parse-failures
     :accessor project-parse-failures
     :reader parse-failures              ;For copying.
@@ -30,3 +32,6 @@ objects."))
 (defmethod asts ((obj parseable-project))
   "Return a list of all ASTs in the project OBJ."
   (apply #'append (mapcar [#'asts #'cdr] (evolve-files obj))))
+
+(defmethod (setf genome) :after (value (self parseable-project))
+  (attrs:invalidate-subroot self))
