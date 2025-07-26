68c886ac1a1960f95346b875fa3a1fac152970b7
Paul M. Rodriguez <prodriguez@grammatech.com>
2025-07-26 14:52:50 -0500

    Use vector for conflict AST descendant map

--- software/parseable.lisp
+++ software/parseable.lisp
@@ -199,10 +199,11 @@ corresponding to that key (default if the key is not present), and splice
 that list of children in place of the conflict node in its parent's children
 list."))
 
-(defmethod slot-unbound ((class t) (node conflict-ast) (slot (eql 'ft:descendant-map)))
+(defmethod slot-unbound
+    ((class t) (node conflict-ast) (slot (eql 'ft:descendant-map)))
   (let ((sn (ft:serial-number node)))
     (setf (slot-value node 'ft:descendant-map)
-          (convert 'ft/it:itree (list (list (cons sn sn) nil))))))
+          (vector (list (cons sn sn) nil)))))
 
 (defparameter *ast-print-cutoff* 20
   "Maximum number of characters to print for TEXT in
