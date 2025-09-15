92f9607fe7e739111a0bbc1828377ee77c25b648
Paul M. Rodriguez <prodriguez@grammatech.com>
2025-09-15 10:19:50 -0500

    Inline variable bindings

--- software/cpp.lisp
+++ software/cpp.lisp
@@ -2275,30 +2275,28 @@ set of possible concrete specializations of that type."
 (defmethod find-enclosing-declaration ((type (eql 'function-declaration-ast))
                                        root
                                        (id cpp-destructor-name))
-  (let ((parents (get-parent-asts* root id)))
-    (match parents
-      ((list*
-        (cpp-function-declarator (cpp-declarator decl-name))
-        (and decl (cpp-declaration))
-        _)
-       (unless (eql decl-name id)
-         (fail))
-       decl)
-      (otherwise (call-next-method)))))
+  (match (get-parent-asts* root id)
+    ((list*
+      (cpp-function-declarator (cpp-declarator decl-name))
+      (and decl (cpp-declaration))
+      _)
+     (unless (eql decl-name id)
+       (fail))
+     decl)
+    (otherwise (call-next-method))))
 
 (defmethod find-enclosing-declaration ((type (eql 'function-declaration-ast))
                                        root
                                        (id cpp-operator-name))
-  (let ((parents (get-parent-asts* root id)))
-    (match parents
-      ((list*
-        (cpp-function-declarator (cpp-declarator decl-name))
-        (and decl (cpp-field-declaration))
-        _)
-       (unless (eql decl-name id)
-         (fail))
-       decl)
-      (otherwise (call-next-method)))))
+  (match (get-parent-asts* root id)
+    ((list*
+      (cpp-function-declarator (cpp-declarator decl-name))
+      (and decl (cpp-field-declaration))
+      _)
+     (unless (eql decl-name id)
+       (fail))
+     decl)
+    (otherwise (call-next-method))))
 
 
 ;;; Whitespace rules
